92e32c8b81eeaf7764aa7c5cb03676bd869eb457
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-04-07 16:43:19 +0300

    Added AppleSkin integration, added client config registration

--- src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
+++ src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
@@ -4,8 +4,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.therealvoin.notjustspoiled.core.config.NJSConfig;
+import net.therealvoin.notjustspoiled.core.config.NJSClientConfig;
+import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
 import net.therealvoin.notjustspoiled.init.NJSGlobalLootModifiers;
+import net.therealvoin.notjustspoiled.integration.AppleSkinIntegration;
 
 @Mod(NotJustSpoiled.MOD_ID)
 public class NotJustSpoiled {
@@ -14,6 +16,9 @@ public class NotJustSpoiled {
     public NotJustSpoiled(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
         NJSGlobalLootModifiers.GLOBAL_LOOT_MODIFIERS_DEFERRED_REGISTER.register(modEventBus);
-        context.registerConfig(ModConfig.Type.SERVER, NJSConfig.CONFIG);
+        context.registerConfig(ModConfig.Type.CLIENT, NJSClientConfig.CLIENT_CONFIG);
+        context.registerConfig(ModConfig.Type.SERVER, NJSServerConfig.SERVER_CONFIG);
+
+        AppleSkinIntegration.init();
     }
 }
