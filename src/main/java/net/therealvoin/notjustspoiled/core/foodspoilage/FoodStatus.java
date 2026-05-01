5a9c756da3530ae2ed6a9d18f7736c59407fae6a
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-01 13:07:45 +0300

    Renamed config constants

--- src/main/java/net/therealvoin/notjustspoiled/core/foodspoilage/FoodStatus.java
+++ src/main/java/net/therealvoin/notjustspoiled/core/foodspoilage/FoodStatus.java
@@ -7,10 +7,10 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
 
 public enum FoodStatus {
-    FRESH("fresh", ChatFormatting.GREEN, NJSServerConfig.CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE),
-    STALE("stale", ChatFormatting.YELLOW, NJSServerConfig.CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE),
-    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, NJSServerConfig.CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE),
-    SPOILED("spoiled", ChatFormatting.RED, NJSServerConfig.CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE);
+    FRESH("fresh", ChatFormatting.GREEN, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE),
+    STALE("stale", ChatFormatting.YELLOW, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE),
+    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE),
+    SPOILED("spoiled", ChatFormatting.RED, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE);
 
     private final String translationKey;
     private final ChatFormatting color;
