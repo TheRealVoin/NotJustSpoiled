9f8a70a5b806f015c9bcfec8451bca886602ec5e
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-01 13:21:36 +0300

    Experimenting...

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
@@ -1,10 +1,13 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.container;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
+import net.therealvoin.notjustspoiled.network.MergeFoodPacket;
+import net.therealvoin.notjustspoiled.network.NJSPacketHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,7 +21,11 @@ public abstract class InventoryMixin {
 
     @Inject(method = "hasRemainingSpaceForItem", at = @At("HEAD"))
     private void makeFoodPossibleForMerging(ItemStack itemStack1, ItemStack itemStack2, CallbackInfoReturnable<Boolean> cir) {
-        FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack2, FoodEnvironment.INVENTORY, player.level());
-        FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack1, itemStack2, player.level());
+        if (player.level() instanceof ServerLevel serverLevel) {
+            FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack2, FoodEnvironment.INVENTORY, serverLevel);
+            FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack1, itemStack2, serverLevel);
+        } else {
+            NJSPacketHandler.CHANNEL.sendToServer(new MergeFoodPacket(itemStack1, itemStack2));
+        }
     }
 }
