bb04c80f6df817badb60a23628607e91a440436f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-01 13:21:11 +0300

    Added fourth loot modifier - SetFreshOrStaleStatus

---
+++ src/main/java/net/therealvoin/notjustspoiled/data/loot/SetFreshOrStaleStatus.java
@@ -0,0 +1,41 @@
+package net.therealvoin.notjustspoiled.data.loot;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.LootModifier;
+import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
+import net.therealvoin.notjustspoiled.core.foodspoilage.capability.foodspoilage.FoodSpoilageProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class SetFreshOrStaleStatus extends LootModifier {
+    public static final Codec<SetFreshOrStaleStatus> CODEC =
+            RecordCodecBuilder.create(instance -> codecStart(instance).apply(instance, SetFreshOrStaleStatus::new));
+
+    protected SetFreshOrStaleStatus(LootItemCondition[] conditionsIn) {
+        super(conditionsIn);
+    }
+
+
+    @Override
+    protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+        for (ItemStack itemStack : generatedLoot) {
+            itemStack.getCapability(FoodSpoilageProvider.FOOD_SPOILAGE).ifPresent(foodSpoilage -> {
+                foodSpoilage.setEnvironment(FoodEnvironment.STORAGE);
+                foodSpoilage.setFoodLifetime(1);
+                foodSpoilage.setLastUpdateTime(context.getLevel().getGameTime());
+            });
+        }
+
+        return generatedLoot;
+    }
+
+    @Override
+    public Codec<? extends IGlobalLootModifier> codec() {
+        return CODEC;
+    }
+}
