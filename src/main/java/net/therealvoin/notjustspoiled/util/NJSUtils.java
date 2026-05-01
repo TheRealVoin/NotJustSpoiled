ba73dbf559fa22cf13a67f82eec50c5054e62821
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-01 13:29:51 +0300

    Added warning message, config improvements

--- src/main/java/net/therealvoin/notjustspoiled/util/NJSUtils.java
+++ src/main/java/net/therealvoin/notjustspoiled/util/NJSUtils.java
@@ -1,5 +1,11 @@
 package net.therealvoin.notjustspoiled.util;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
@@ -11,12 +17,17 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.therealvoin.notjustspoiled.core.config.NJSConfig;
+import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodCategory;
+import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import net.therealvoin.notjustspoiled.core.foodspoilage.capability.foodspoilage.FoodSpoilageProvider;
 import net.therealvoin.notjustspoiled.core.foodspoilage.capability.foodspoilage.IFoodSpoilage;
 
+import java.util.List;
+
 public class NJSUtils {
+    private static long lastWarningMessageTime = 0;
+
     public static Level getLevelBySlot(Slot slot) {
         Container container = slot.container;
         if (container instanceof Inventory inventory) {
@@ -59,14 +70,14 @@ public class NJSUtils {
     }
 
     public static void validateChances(ModConfigEvent event) {
-        if (event.getConfig().getSpec() != NJSConfig.CONFIG) {
+        if (event.getConfig().getSpec() != NJSServerConfig.SERVER_CONFIG) {
             return;
         }
 
-        double fresh = NJSConfig.CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.get();
-        double stale = NJSConfig.CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.get();
-        double halfSpoiled = NJSConfig.CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE.get();
-        double spoiled = NJSConfig.CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE.get();
+        double fresh = NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.get();
+        double stale = NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.get();
+        double halfSpoiled = NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE.get();
+        double spoiled = NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE.get();
         double remaining = 1.0;
 
         fresh = Math.min(fresh, remaining);
@@ -82,9 +93,40 @@ public class NJSUtils {
             fresh += remaining;
         }
 
-        NJSConfig.CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.set(fresh);
-        NJSConfig.CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.set(stale);
-        NJSConfig.CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE.set(halfSpoiled);
-        NJSConfig.CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE.set(spoiled);
+        NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.set(fresh);
+        NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.set(stale);
+        NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE.set(halfSpoiled);
+        NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE.set(spoiled);
+
+
+        fresh = NJSServerConfig.FRESH_OR_STALE$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.get();
+        stale = NJSServerConfig.FRESH_OR_STALE$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.get();
+        remaining = 1.0;
+
+        fresh = Math.min(fresh, remaining);
+        remaining -= fresh;
+        stale = Math.min(stale, remaining);
+        remaining -= stale;
+
+        if (remaining > 0) {
+            fresh += remaining;
+        }
+
+        NJSServerConfig.FRESH_OR_STALE$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.set(fresh);
+        NJSServerConfig.FRESH_OR_STALE$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE.set(stale);
+    }
+
+    public static void sendWarningMessage(ServerLevel level) {
+        List<ServerPlayer> players = level.getServer().getPlayerList().getPlayers();
+        if (!players.isEmpty()) {
+            long currentGameTime = level.getGameTime();
+            if (currentGameTime == lastWarningMessageTime) {
+                return;
+            }
+            Component modName = Component.translatable("message.notjustspoiled.warning_message.mod_name").withStyle(ChatFormatting.BOLD);
+            Component issuesPage = Component.translatable("message.notjustspoiled.warning_message.issues_page").withStyle(Style.EMPTY.withUnderlined(true).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/TheRealVoin/NotJustSpoiled/issues")));
+            players.get(0).sendSystemMessage(Component.translatable("message.notjustspoiled.warning_message", modName, issuesPage));
+            lastWarningMessageTime = currentGameTime;
+        }
     }
 }
