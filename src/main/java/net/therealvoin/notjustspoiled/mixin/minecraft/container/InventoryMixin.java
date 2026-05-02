b9282717a15a1900c709c5c44a52810fad9de7ac
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-02 09:59:09 +0300

    Experimenting...

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
@@ -6,26 +6,23 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
-import net.therealvoin.notjustspoiled.network.MergeFoodPacket;
-import net.therealvoin.notjustspoiled.network.NJSPacketHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Inventory.class)
 public abstract class InventoryMixin {
     @Shadow @Final public Player player;
 
-    @Inject(method = "hasRemainingSpaceForItem", at = @At("HEAD"))
-    private void makeFoodPossibleForMerging(ItemStack itemStack1, ItemStack itemStack2, CallbackInfoReturnable<Boolean> cir) {
+    @Redirect(method = "hasRemainingSpaceForItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private boolean makeFoodPossibleForMerging(ItemStack itemStack1, ItemStack itemStack2) {
         if (player.level() instanceof ServerLevel serverLevel) {
             FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack2, FoodEnvironment.INVENTORY, serverLevel);
-            FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack1, itemStack2, serverLevel);
-        } else {
-            NJSPacketHandler.CHANNEL.sendToServer(new MergeFoodPacket(itemStack1, itemStack2));
+            FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack1, itemStack1, serverLevel);
         }
+
+        return ItemStack.isSameItemSameTags(itemStack1, itemStack2);
     }
 }
