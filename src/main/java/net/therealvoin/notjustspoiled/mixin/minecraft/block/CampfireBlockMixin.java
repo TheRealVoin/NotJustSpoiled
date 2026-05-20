c3527564773bf930f70626170e809bb477873499
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-20 20:54:34 +0300

    Various code and variable names improvements

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockMixin.java
@@ -1,5 +1,6 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.block;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
@@ -21,24 +22,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CampfireBlock.class)
 public abstract class CampfireBlockMixin {
     @Inject(method = "dowse", at = @At("TAIL"))
-    private static void changeFoodEnvironmentWhenCampfireExtinguished(Entity entity, LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-        if (levelAccessor instanceof ServerLevel serverLevel) {
-            CampfireBlockEntity campfire = (CampfireBlockEntity) serverLevel.getBlockEntity(blockPos);
+    private static void changeFoodEnvironmentWhenCampfireExtinguished(CallbackInfo ci, @Local(argsOnly = true) LevelAccessor levelAccessor, @Local(argsOnly = true) BlockPos blockPos) {
+        if (levelAccessor instanceof Level level) {
+            CampfireBlockEntity campfire = (CampfireBlockEntity) level.getBlockEntity(blockPos);
 
-            for (ItemStack itemStack : campfire.getItems()) {
-                FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack, FoodEnvironment.GROUND, serverLevel);
+            for (ItemStack foodStack : campfire.getItems()) {
+                FoodSpoilageManager.changeEnvironmentAndUpdate(foodStack, FoodEnvironment.GROUND, level);
             }
         }
     }
 
     @Inject(method = "onProjectileHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", shift = At.Shift.AFTER))
-    private void changeFoodEnvironmentWhenCampfireIgnitedByProjectile(Level level, BlockState blockState, BlockHitResult hitResult, Projectile projectile, CallbackInfo ci) {
-        if (level instanceof ServerLevel serverLevel) {
-            CampfireBlockEntity campfire = (CampfireBlockEntity) serverLevel.getBlockEntity(hitResult.getBlockPos());
+    private void changeFoodEnvironmentWhenCampfireIgnitedByProjectile(CallbackInfo ci, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockHitResult hitResult) {
+        CampfireBlockEntity campfire = (CampfireBlockEntity) level.getBlockEntity(hitResult.getBlockPos());
 
-            for (ItemStack itemStack : campfire.getItems()) {
-                FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack, FoodEnvironment.COOKING, serverLevel);
-            }
+        for (ItemStack foodStack : campfire.getItems()) {
+            FoodSpoilageManager.changeEnvironmentAndUpdate(foodStack, FoodEnvironment.COOKING, level);
         }
     }
 }
