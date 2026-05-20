acd6188a4da9b650904d3717f50ee6ceb564bc09
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-20 21:04:25 +0300

    Fixed issue, where while placing food on campfire, the hole stack was updated

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
@@ -1,14 +1,12 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.Container;
-import net.minecraft.world.entity.Entity;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import net.therealvoin.notjustspoiled.util.NJSUtils;
@@ -16,24 +14,21 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(CampfireBlockEntity.class)
 public abstract class CampfireBlockEntityMixin {
-    @Inject(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void copyCapabilityToCookedFood(Level level, BlockPos blockPos, BlockState blockState, CampfireBlockEntity campfireBlockEntity, CallbackInfo ci, boolean flag, int i, ItemStack inputItem, int j, Container container, ItemStack resultItem) {
-        if (level instanceof ServerLevel serverLevel) {
-            NJSUtils.copyCapability(inputItem, resultItem, serverLevel);
-        }
+    @Inject(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.BEFORE))
+    private static void copySpoilageCapToCookedFood(CallbackInfo ci, @Local(argsOnly = true) Level level, @Local(name = "itemstack") ItemStack inputItem, @Local(name = "itemstack1") ItemStack resultItem) {
+        NJSUtils.copyCapability(inputItem, resultItem, level);
     }
 
-    @Inject(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;split(I)Lnet/minecraft/world/item/ItemStack;"))
-    private void changeFoodEnvironmentWhenPlacedOnCampfire(Entity entity, ItemStack itemStack, int cookingTime, CallbackInfoReturnable<Boolean> cir) {
-        if (entity != null && entity.level() instanceof ServerLevel serverLevel) {
-            CampfireBlockEntity campfire = (CampfireBlockEntity) (Object) this;
-            FoodEnvironment environment = campfire.getBlockState().getValue(CampfireBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.GROUND;
-            FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack, environment, serverLevel);
-        }
+    @WrapOperation(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;split(I)Lnet/minecraft/world/item/ItemStack;"))
+    private ItemStack changeFoodEnvironmentWhenPlacedOnCampfire(ItemStack stackInHand, int amount, Operation<ItemStack> original) {
+        ItemStack stackToPlaceOnCampfire = original.call(stackInHand, amount);
+        CampfireBlockEntity campfire = (CampfireBlockEntity) (Object) this;
+        FoodEnvironment foodEnvironment = campfire.getBlockState().getValue(CampfireBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.GROUND;
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnCampfire, foodEnvironment, campfire.getLevel());
+
+        return stackToPlaceOnCampfire;
     }
 }
