4b64f5a2e383754f9f1944bf9f6be52e59daa751
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-20 17:22:32 +0300

    Improved compatibility with Alex's Mobs

---
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/forge/SidedInvWrapperMixin.java
@@ -0,0 +1,24 @@
+package net.therealvoin.notjustspoiled.mixin.forge;
+
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
+import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(value = SidedInvWrapper.class, remap = false)
+public abstract class SidedInvWrapperMixin {
+    @Shadow @Final protected WorldlyContainer inv;
+
+    @ModifyArgs(method = "insertItem", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemHandlerHelper;canItemStacksStack(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private void mergeFood(Args args) {
+        if (this.inv instanceof BlockEntity blockEntity) {
+            FoodSpoilageManager.tryAverageSpoilageOnMerge(args.get(0), args.get(1), blockEntity.getLevel());
+        }
+    }
+}
