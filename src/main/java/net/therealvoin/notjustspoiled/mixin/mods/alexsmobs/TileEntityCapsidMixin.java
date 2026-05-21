00d556ae2c25116d6b8d7dbac9a2fac56e8a042b
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-21 20:53:35 +0300

    Simplified mixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
@@ -11,18 +11,17 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Pseudo
 @Mixin(targets = "com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid")
 public abstract class TileEntityCapsidMixin {
-    @Inject(method = "tick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;copy()Lnet/minecraft/world/item/ItemStack;", ordinal = 0, shift = At.Shift.AFTER), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V")))
+    @Inject(method = "tick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;copy()Lnet/minecraft/world/item/ItemStack;", ordinal = 1, shift = At.Shift.AFTER))
     private void createStackInCapsidSnapshot(CallbackInfo ci, @Local(name = "current") ItemStack stackInCapsid, @Share("stackInCapsidSnapshot") LocalRef<ItemStack> snapshot) {
         snapshot.set(stackInCapsid.copy());
     }
 
-    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/tileentity/TileEntityCapsid;setItem(ILnet/minecraft/world/item/ItemStack;)V"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V")), index = 1)
+    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/tileentity/TileEntityCapsid;setItem(ILnet/minecraft/world/item/ItemStack;)V", ordinal = 2), index = 1)
     private ItemStack copySpoilageCapToResultStack(ItemStack stackToSetInCapsid, @Share("stackInCapsidSnapshot") LocalRef<ItemStack> snapshot) {
         NJSUtils.copyCapability(snapshot.get(), stackToSetInCapsid, ((BlockEntity)(Object)this).getLevel());
         return stackToSetInCapsid;
