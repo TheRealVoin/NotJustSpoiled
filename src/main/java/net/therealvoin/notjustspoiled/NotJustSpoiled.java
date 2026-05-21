c89bf99e43dcc5c24585f79c464a410ebdaab0b8
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-21 20:54:18 +0300

    Removed packets, added mod name constant

--- src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
+++ src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
@@ -8,18 +8,17 @@ import net.therealvoin.notjustspoiled.core.config.NJSClientConfig;
 import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
 import net.therealvoin.notjustspoiled.init.NJSGlobalLootModifiers;
 import net.therealvoin.notjustspoiled.integration.AppleSkinIntegration;
-import net.therealvoin.notjustspoiled.network.NJSPacketHandler;
 
 @Mod(NotJustSpoiled.MOD_ID)
 public class NotJustSpoiled {
     public static final String MOD_ID = "notjustspoiled";
+    public static final String MOD_NAME = "Not Just Spoiled";
 
     public NotJustSpoiled(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
         NJSGlobalLootModifiers.GLOBAL_LOOT_MODIFIERS_DEFERRED_REGISTER.register(modEventBus);
         context.registerConfig(ModConfig.Type.CLIENT, NJSClientConfig.CLIENT_CONFIG);
         context.registerConfig(ModConfig.Type.SERVER, NJSServerConfig.SERVER_CONFIG);
-        NJSPacketHandler.register();
 
         AppleSkinIntegration.init();
     }
