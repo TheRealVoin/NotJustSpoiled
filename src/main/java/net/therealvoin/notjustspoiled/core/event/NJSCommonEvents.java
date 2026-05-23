a046381dd2d2f76d5dd810fb17dd7b3e530b04ba
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-23 11:14:15 +0300

    Moved client events into separate class NJSClientEvents, various code improvements

--- src/main/java/net/therealvoin/notjustspoiled/core/event/NJSCommonEvents.java
+++ src/main/java/net/therealvoin/notjustspoiled/core/event/NJSCommonEvents.java
@@ -1,6 +1,5 @@
-package net.therealvoin.notjustspoiled.core;
+package net.therealvoin.notjustspoiled.core.event;
 
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -10,15 +9,14 @@ import net.minecraft.server.packs.PathPackResources;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.ItemStackedOnOtherEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -26,71 +24,21 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.therealvoin.notjustspoiled.NotJustSpoiled;
 import net.therealvoin.notjustspoiled.core.config.FoodCraftingMode;
-import net.therealvoin.notjustspoiled.core.config.NJSClientConfig;
 import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodCategory;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
-import net.therealvoin.notjustspoiled.core.foodspoilage.FoodStatus;
 import net.therealvoin.notjustspoiled.core.foodspoilage.capability.blockfoodspoilage.BlockFoodSpoilageProvider;
 import net.therealvoin.notjustspoiled.core.foodspoilage.capability.foodspoilage.FoodSpoilageProvider;
 import net.therealvoin.notjustspoiled.core.foodspoilage.capability.foodspoilage.IFoodSpoilage;
+import net.therealvoin.notjustspoiled.util.FoodFinderUtil;
 import net.therealvoin.notjustspoiled.util.NJSUtils;
 
 import java.nio.file.Path;
-import java.util.List;
 
-public class NJSEvents {
-    @Mod.EventBusSubscriber(modid = NotJustSpoiled.MOD_ID, value = Dist.CLIENT)
-    public static class Client {
-        private static final Component STATUS = Component.translatable("tooltip.notjustspoiled.status").append(": ").withStyle(ChatFormatting.GRAY);
-        private static final Component NEVER_SPOILS = Component.translatable("tooltip.notjustspoiled.never_spoils").withStyle(ChatFormatting.AQUA);
-        private static final Component CATEGORY = Component.translatable("tooltip.notjustspoiled.debug.category").append(": ").withStyle(ChatFormatting.GRAY);
-
-        @SubscribeEvent
-        public static void addFoodStatusToTooltip(ItemTooltipEvent event) {
-            ItemStack tooltipItem = event.getItemStack();
-            List<Component> tooltip = event.getToolTip();
-
-            if (tooltipItem.is(NJSTags.Items.ALWAYS_SPOILED)) {
-                tooltip.add(STATUS.copy().append(FoodStatus.SPOILED.getTranslation()));
-                return;
-            } else if (tooltipItem.is(NJSTags.Items.NEVER_SPOILS)) {
-                tooltip.add(STATUS.copy().append(NEVER_SPOILS));
-                return;
-            }
-
-            if (NJSClientConfig.SHOW_CATEGORY_IN_TOOLTIP.get()) {
-                FoodCategory foodCategory = FoodCategory.getFoodCategory(tooltipItem);
-                Component category = Component.literal(String.valueOf(foodCategory)).withStyle(ChatFormatting.LIGHT_PURPLE);
-                if (foodCategory == null) {
-                    category = category.copy().withStyle(ChatFormatting.GOLD);
-                }
-                tooltip.add(CATEGORY.copy().append(category));
-            }
-
-            if (event.getEntity() == null) {
-                return;
-            }
-
-            FoodStatus currentFoodStatus = FoodSpoilageManager.getFoodStatus(tooltipItem, event.getEntity().level());
-            if (currentFoodStatus == null) {
-                return;
-            }
-
-            tooltip.add(STATUS.copy().append(currentFoodStatus.getTranslation()));
-
-            if (NJSClientConfig.SHOW_ADDITIONAL_INFO.get()) {
-                IFoodSpoilage foodSpoilage = NJSUtils.getCapability(tooltipItem);
-                tooltip.add(Component.literal("lastUpdateTime: ").append(String.valueOf(foodSpoilage.getLastUpdateTime())));
-                tooltip.add(Component.literal("foodLifetime: ").append(String.valueOf(foodSpoilage.getFoodLifetime())));
-                tooltip.add(Component.literal("environment: ").append(foodSpoilage.getEnvironment().name()));
-            }
-        }
-    }
-
+public class NJSCommonEvents {
     @Mod.EventBusSubscriber(modid = NotJustSpoiled.MOD_ID)
-    public static class CommonForge {
+    public static class ForgeBus {
         private static final ResourceLocation FOOD_SPOILAGE = ResourceLocation.fromNamespaceAndPath(NotJustSpoiled.MOD_ID, "food_spoilage");
         private static final ResourceLocation BLOCK_FOOD_SPOILAGE = ResourceLocation.fromNamespaceAndPath(NotJustSpoiled.MOD_ID, "block_food_spoilage");
 
@@ -104,7 +52,7 @@ public class NJSEvents {
 
                 ItemStack itemEntityStack = itemEntity.getItem();
 
-                serverLevel.getCapability(BlockFoodSpoilageProvider.BLOCK_FOOD_SPOILAGE).ifPresent(blockFoodSpoilage ->{
+                serverLevel.getCapability(BlockFoodSpoilageProvider.BLOCK_FOOD_SPOILAGE).ifPresent(blockFoodSpoilage -> {
                     BlockPos blockPos = itemEntity.blockPosition();
                     ItemStack itemStack = blockFoodSpoilage.getLastItemStackByPos(blockPos);
                     if (itemStack.isEmpty()) {
@@ -123,19 +71,17 @@ public class NJSEvents {
 
         @SubscribeEvent
         public static void onItemStackedOnOther(ItemStackedOnOtherEvent event) {
-            if (!(event.getPlayer().level() instanceof ServerLevel serverLevel)) {
-                return;
-            }
+            if (event.getPlayer().level() instanceof ServerLevel serverLevel) {
+                ItemStack carriedItem = event.getStackedOnItem();
+                ItemStack stackedOnItem = event.getCarriedItem();
 
-            ItemStack carriedItem = event.getCarriedItem();
-            ItemStack stackedOnItem = event.getStackedOnItem();
+                if (!carriedItem.isEmpty() && !stackedOnItem.isEmpty()) {
+                    stackedOnItem.getCapability(FoodSpoilageProvider.FOOD_SPOILAGE).ifPresent(foodSpoilage -> {
+                        FoodSpoilageManager.changeEnvironmentAndUpdate(carriedItem, foodSpoilage.getEnvironment(), serverLevel);
+                    });
 
-            if (!carriedItem.isEmpty() && !stackedOnItem.isEmpty()) {
-                carriedItem.getCapability(FoodSpoilageProvider.FOOD_SPOILAGE).ifPresent(foodSpoilage -> {
-                    FoodSpoilageManager.changeEnvironmentAndUpdate(stackedOnItem, foodSpoilage.getEnvironment(), serverLevel);
-                });
-
-                FoodSpoilageManager.tryAverageSpoilageOnMerge(stackedOnItem, carriedItem, serverLevel);
+                    FoodSpoilageManager.tryAverageSpoilageOnMerge(carriedItem, stackedOnItem, serverLevel);
+                }
             }
         }
 
@@ -221,11 +167,9 @@ public class NJSEvents {
         // Capability events
         @SubscribeEvent
         public static void attachFoodSpoilageToFood(AttachCapabilitiesEvent<ItemStack> event) {
-            if (FoodCategory.getFoodCategory(event.getObject()) == null) {
-                return;
+            if (FoodCategory.getFoodCategory(event.getObject()) != null) {
+                event.addCapability(FOOD_SPOILAGE, new FoodSpoilageProvider());
             }
-
-            event.addCapability(FOOD_SPOILAGE, new FoodSpoilageProvider());
         }
 
         @SubscribeEvent
@@ -237,7 +181,7 @@ public class NJSEvents {
     }
 
     @Mod.EventBusSubscriber(modid = NotJustSpoiled.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class CommonMod {
+    public static class ModBus {
         // Config events
         @SubscribeEvent
         public static void onConfigLoad(ModConfigEvent.Loading event) {
@@ -249,6 +193,7 @@ public class NJSEvents {
             NJSUtils.validateChances(event);
         }
 
+        // Datapack event
         @SubscribeEvent
         public static void addCustomDatapack(AddPackFindersEvent event) {
             if (event.getPackType() != PackType.SERVER_DATA) {
@@ -256,16 +201,19 @@ public class NJSEvents {
             }
 
             Path path = ModList.get().getModFileById(NotJustSpoiled.MOD_ID).getFile().findResource("datapack");
-            Pack.ResourcesSupplier supplier = id -> new PathPackResources(NotJustSpoiled.MOD_ID, path, false);
+            Pack.ResourcesSupplier supplier = id -> new PathPackResources(NotJustSpoiled.MOD_ID, path, true);
+            Pack.Info info = new Pack.Info(Component.translatable("datapack.notjustspoiled.description"), 15, 15, FeatureFlagSet.of(), true);
 
-            Pack pack = Pack.readMetaAndCreate(
+            Pack pack =  Pack.create(
                     NotJustSpoiled.MOD_ID,
-                    Component.literal("Not Just Spoiled"),
+                    Component.literal(NotJustSpoiled.MOD_NAME),
                     true,
                     supplier,
+                    info,
                     PackType.SERVER_DATA,
                     Pack.Position.TOP,
-                    PackSource.DEFAULT
+                    true,
+                    PackSource.BUILT_IN
             );
 
             event.addRepositorySource(source -> source.accept(pack));
