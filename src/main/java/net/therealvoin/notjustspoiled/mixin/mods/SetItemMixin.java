69e9d5b9aa934a19ba5e4441f8c1f8498bbbbaa4
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-23 12:09:49 +0300

    Improved SetItemMixin a lot, added compatibility with Alex's Caves, Alex's Mobs and Tough As Nails

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/SetItemMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/SetItemMixin.java
@@ -1,28 +1,25 @@
 package net.therealvoin.notjustspoiled.mixin.mods;
 
-import dev.enemeez.simplefarming.common.block.entity.FermenterBlockEntity;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.*;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import vectorwing.farmersdelight.common.block.entity.BasketBlockEntity;
 
-@Mixin(value = {BasketBlockEntity.class, FermenterBlockEntity.class}, remap = false)
+@Pseudo
+@Mixin(targets = {
+        "com.github.alexmodguy.alexscaves.server.block.blockentity.NuclearFurnaceBlockEntity",
+        "com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid",
+        "dev.enemeez.simplefarming.common.block.entity.FermenterBlockEntity",
+        "toughasnails.block.entity.WaterPurifierBlockEntity"
+}, remap = false)
 public abstract class SetItemMixin {
-    @Inject(method = {"setItem", "m_6836_"}, at = @At("HEAD"))
-    private void changeFoodEnvironmentWhenPlacedInContainer(int index, ItemStack itemStack, CallbackInfo ci) {
-        BlockEntity blockEntity = (BlockEntity) (Object) this;
-        Level level = blockEntity.getLevel();
-        FoodEnvironment foodEnvironment = FoodEnvironment.STORAGE;
-
-        if (level instanceof ServerLevel serverLevel && !itemStack.isEmpty()) {
-            FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack, foodEnvironment, serverLevel);
-        }
+    @Inject(method = {"setItem", "m_6836_"}, at = @At("TAIL"))
+    private void changeFoodEnvironmentWhenPlacedInContainer(int index, ItemStack stackToSetInStorage, CallbackInfo ci) {
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToSetInStorage, FoodEnvironment.STORAGE, ((BlockEntity)(Object)this).getLevel());
     }
 }
