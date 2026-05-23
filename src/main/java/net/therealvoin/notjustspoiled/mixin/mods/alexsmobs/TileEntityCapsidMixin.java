d639bf62424b7b6d7b58c1c7c6d053fc16c7d6c7
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-23 11:03:45 +0300

    Fixed issue, where game works in dev environment and crashes in runtime

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
@@ -21,7 +21,8 @@ public abstract class TileEntityCapsidMixin {
         snapshot.set(stackInCapsid.copy());
     }
 
-    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/tileentity/TileEntityCapsid;setItem(ILnet/minecraft/world/item/ItemStack;)V", ordinal = 2), index = 1)
+    // You should replace m_6836_ with setItem to work in dev environment
+    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/tileentity/TileEntityCapsid;m_6836_(ILnet/minecraft/world/item/ItemStack;)V", ordinal = 2), index = 1)
     private ItemStack copySpoilageCapToResultStack(ItemStack stackToSetInCapsid, @Share("stackInCapsidSnapshot") LocalRef<ItemStack> snapshot) {
         NJSUtils.copyCapability(snapshot.get(), stackToSetInCapsid, ((BlockEntity)(Object)this).getLevel());
         return stackToSetInCapsid;
