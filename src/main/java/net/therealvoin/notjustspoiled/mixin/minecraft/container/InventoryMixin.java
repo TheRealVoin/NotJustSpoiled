3885efe21038e9a25fe606bc273938c9d9135fad
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-23 12:03:28 +0300

    Fixed many bugs, related to incorrect mixin injection logic

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
@@ -3,6 +3,7 @@ package net.therealvoin.notjustspoiled.mixin.minecraft.container;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
@@ -10,19 +11,19 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Inventory.class)
 public abstract class InventoryMixin {
     @Shadow @Final public Player player;
 
-    @Redirect(method = "hasRemainingSpaceForItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
-    private boolean makeFoodPossibleForMerging(ItemStack itemStack1, ItemStack itemStack2) {
+    @Inject(method = "addResource(ILnet/minecraft/world/item/ItemStack;)I", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/player/Inventory;getItem(I)Lnet/minecraft/world/item/ItemStack;"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void updateFood(int pSlot, ItemStack pStack, CallbackInfoReturnable<Integer> cir, Item item, int i, ItemStack itemStack) {
         if (player.level() instanceof ServerLevel serverLevel) {
-            FoodSpoilageManager.changeEnvironmentAndUpdate(itemStack2, FoodEnvironment.INVENTORY, serverLevel);
-            FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack1, itemStack1, serverLevel);
+            FoodSpoilageManager.changeEnvironmentAndUpdate(pStack, FoodEnvironment.INVENTORY, serverLevel);
+            FoodSpoilageManager.tryAverageSpoilageOnMerge(itemStack, pStack, serverLevel);
         }
-
-        return ItemStack.isSameItemSameTags(itemStack1, itemStack2);
     }
 }
