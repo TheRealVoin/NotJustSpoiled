e6bcccb994a19f80b94689fcc86c2992bdb975b4
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 16:32:18 +0300

    Added negative effects to half-spoiled and spoiled food

--- src/main/java/net/therealvoin/notjustspoiled/core/foodspoilage/FoodStatus.java
+++ src/main/java/net/therealvoin/notjustspoiled/core/foodspoilage/FoodStatus.java
@@ -3,23 +3,65 @@ package net.therealvoin.notjustspoiled.core.foodspoilage;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.therealvoin.notjustspoiled.core.config.NJSServerConfig;
+
+import java.util.List;
 
 public enum FoodStatus {
     FRESH("fresh", ChatFormatting.GREEN, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE),
     STALE("stale", ChatFormatting.YELLOW, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE),
-    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE),
-    SPOILED("spoiled", ChatFormatting.RED, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE);
+    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE,
+            new EffectData(MobEffects.POISON,
+                    NJSServerConfig.POISON_EFFECT_FOR_HALF_SPOILED_FOOD_DURATION,
+                    0,
+                    NJSServerConfig.CHANCE_TO_APPLY_POISON_EFFECT_FOR_HALF_SPOILED_FOOD
+            ),
+            new EffectData(MobEffects.HUNGER,
+                    NJSServerConfig.HUNGER_EFFECT_FOR_HALF_SPOILED_FOOD_DURATION,
+                    0,
+                    NJSServerConfig.CHANCE_TO_APPLY_HUNGER_EFFECT_FOR_HALF_SPOILED_FOOD
+            ),
+            new EffectData(MobEffects.CONFUSION,
+                    NJSServerConfig.NAUSEA_EFFECT_FOR_HALF_SPOILED_FOOD_DURATION,
+                    0,
+                    NJSServerConfig.CHANCE_TO_APPLY_NAUSEA_EFFECT_FOR_HALF_SPOILED_FOOD
+            )
+    ),
+    SPOILED("spoiled", ChatFormatting.RED, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE,
+            new EffectData(
+                    MobEffects.POISON,
+                    NJSServerConfig.POISON_EFFECT_FOR_SPOILED_FOOD_DURATION,
+                    0,
+                    NJSServerConfig.CHANCE_TO_APPLY_POISON_EFFECT_FOR_SPOILED_FOOD
+            ),
+            new EffectData(
+                    MobEffects.HUNGER,
+                    NJSServerConfig.HUNGER_EFFECT_FOR_SPOILED_FOOD_DURATION,
+                    1,
+                    NJSServerConfig.CHANCE_TO_APPLY_HUNGER_EFFECT_FOR_SPOILED_FOOD
+            ),
+            new EffectData(
+                    MobEffects.CONFUSION,
+                    NJSServerConfig.NAUSEA_EFFECT_FOR_SPOILED_FOOD_DURATION,
+                    0,
+                    NJSServerConfig.CHANCE_TO_APPLY_NAUSEA_EFFECT_FOR_SPOILED_FOOD
+            )
+    );
 
     private final String translationKey;
     private final ChatFormatting color;
     private final ForgeConfigSpec.DoubleValue chanceToAppearInStorage;
+    private final List<EffectData> effectsData;
 
-    FoodStatus(String translationKey, ChatFormatting color, ForgeConfigSpec.DoubleValue chanceToAppearInStorage) {
+    FoodStatus(String translationKey, ChatFormatting color, ForgeConfigSpec.DoubleValue chanceToAppearInStorage, EffectData... effectsData) {
         this.translationKey = translationKey;
         this.color = color;
         this.chanceToAppearInStorage = chanceToAppearInStorage;
+        this.effectsData = List.of(effectsData);
     }
 
     public Component getTranslation() {
@@ -139,6 +181,34 @@ public enum FoodStatus {
             return HALF_SPOILED;
         } else {
             return SPOILED;
+        }
+    }
+
+    public List<EffectData> getEffectsData() {
+        return this.effectsData;
+    }
+
+
+    public static class EffectData {
+        private final MobEffect effect;
+        private final ForgeConfigSpec.IntValue duration;
+        private final int amplifier;
+        private final ForgeConfigSpec.DoubleValue applyChance;
+
+
+        public EffectData(MobEffect effect, ForgeConfigSpec.IntValue duration, int amplifier, ForgeConfigSpec.DoubleValue applyChance) {
+            this.effect = effect;
+            this.duration = duration;
+            this.amplifier = amplifier;
+            this.applyChance = applyChance;
+        }
+
+        public MobEffectInstance createEffectInstance() {
+            return new MobEffectInstance(this.effect, this.duration.get(), this.amplifier, false, false);
+        }
+
+        public double getApplyChance() {
+            return this.applyChance.get();
         }
     }
 }
