50466635ce34452726bb285a2d1828f6cc9287ab
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 16:39:19 +0300

    Improved SetItemMixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/SetItemMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/SetItemMixin.java
@@ -1,21 +1,25 @@
 package net.therealvoin.notjustspoiled.mixin.mods;
 
+import com.github.alexmodguy.alexscaves.server.block.blockentity.NuclearFurnaceBlockEntity;
+import com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid;
+import dev.enemeez.simplefarming.common.block.entity.FermenterBlockEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import toughasnails.block.entity.WaterPurifierBlockEntity;
+import vectorwing.farmersdelight.common.block.entity.BasketBlockEntity;
 
-@Pseudo
-@Mixin(targets = {
-        "com.github.alexmodguy.alexscaves.server.block.blockentity.NuclearFurnaceBlockEntity",
-        "com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid",
-        "dev.enemeez.simplefarming.common.block.entity.FermenterBlockEntity",
-        "toughasnails.block.entity.WaterPurifierBlockEntity"
+@Mixin(value = {
+        NuclearFurnaceBlockEntity.class,
+        TileEntityCapsid.class,
+        BasketBlockEntity.class,
+        FermenterBlockEntity.class,
+        WaterPurifierBlockEntity.class
 }, remap = false)
 public abstract class SetItemMixin {
     @Inject(method = {"setItem", "m_6836_"}, at = @At("TAIL"))
