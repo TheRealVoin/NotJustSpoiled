e7c1ee123abb1f4cb845e64ed5f1ec3cc83a6d0f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 15:38:22 +0300

    Improved TileEntityCapsidMixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/TileEntityCapsidMixin.java
@@ -1,5 +1,6 @@
 package net.therealvoin.notjustspoiled.mixin.mods.alexsmobs;
 
+import com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
@@ -7,21 +8,18 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.therealvoin.notjustspoiled.util.NJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Pseudo
-@Mixin(targets = "com.github.alexthe666.alexsmobs.tileentity.TileEntityCapsid")
+@Mixin(TileEntityCapsid.class)
 public abstract class TileEntityCapsidMixin {
     @Inject(method = "tick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;copy()Lnet/minecraft/world/item/ItemStack;", ordinal = 1, shift = At.Shift.AFTER))
     private void createStackInCapsidSnapshot(CallbackInfo ci, @Local(name = "current") ItemStack stackInCapsid, @Share("stackInCapsidSnapshot") LocalRef<ItemStack> snapshot) {
         snapshot.set(stackInCapsid.copy());
     }
 
-    // You should replace m_6836_ with setItem to work in dev environment
     @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/tileentity/TileEntityCapsid;setItem(ILnet/minecraft/world/item/ItemStack;)V", ordinal = 2), index = 1)
     private ItemStack copySpoilageCapToResultStack(ItemStack stackToSetInCapsid, @Share("stackInCapsidSnapshot") LocalRef<ItemStack> snapshot) {
         NJSUtils.copyCapability(snapshot.get(), stackToSetInCapsid, ((BlockEntity)(Object)this).getLevel());
