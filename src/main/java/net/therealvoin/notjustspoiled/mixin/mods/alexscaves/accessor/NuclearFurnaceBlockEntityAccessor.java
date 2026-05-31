51fd5f0cc02f901560cb341bf974899446f82230
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 16:28:22 +0300

    Improved NuclearFurnaceBlockEntityAccessor

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexscaves/accessor/NuclearFurnaceBlockEntityAccessor.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexscaves/accessor/NuclearFurnaceBlockEntityAccessor.java
@@ -1,11 +1,10 @@
 package net.therealvoin.notjustspoiled.mixin.mods.alexscaves.accessor;
 
+import com.github.alexmodguy.alexscaves.server.block.blockentity.NuclearFurnaceBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Pseudo
-@Mixin(targets = "com.github.alexmodguy.alexscaves.server.block.blockentity.NuclearFurnaceBlockEntity", remap = false)
+@Mixin(value = NuclearFurnaceBlockEntity.class, remap = false)
 public interface NuclearFurnaceBlockEntityAccessor {
     @Accessor int getCookTime();
 }
