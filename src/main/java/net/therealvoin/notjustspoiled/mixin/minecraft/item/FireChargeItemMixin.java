a9eec842466dd8a5f717b0b2ebb33120fe4c1706
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 16:36:11 +0300

    Removed unused imports

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/item/FireChargeItemMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/item/FireChargeItemMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.FireChargeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
