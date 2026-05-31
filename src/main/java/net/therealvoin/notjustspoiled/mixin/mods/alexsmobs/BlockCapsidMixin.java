560853ce1f31baa46c106d0d08532003d9a5859f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 15:37:20 +0300

    Improved BlockCapsidMixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/BlockCapsidMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexsmobs/BlockCapsidMixin.java
@@ -1,5 +1,6 @@
 package net.therealvoin.notjustspoiled.mixin.mods.alexsmobs;
 
+import com.github.alexthe666.alexsmobs.block.BlockCapsid;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -8,13 +9,11 @@ import net.minecraft.world.level.Level;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Pseudo
-@Mixin(targets = "com.github.alexthe666.alexsmobs.block.BlockCapsid")
+@Mixin(BlockCapsid.class)
 public abstract class BlockCapsidMixin {
-    @WrapOperation(method = {"use", "m_6227_"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
+    @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
     private void mergeFood(ItemStack stackInCapsid, int increment, Operation<Void> original, @Local(argsOnly = true) Level level, @Local(name = "copy") ItemStack stackInHand) {
         FoodSpoilageManager.changeEnvironmentAndUpdate(stackInHand, FoodEnvironment.STORAGE, level);
         FoodSpoilageManager.tryAverageSpoilageOnMerge(stackInCapsid, stackInHand, level);
