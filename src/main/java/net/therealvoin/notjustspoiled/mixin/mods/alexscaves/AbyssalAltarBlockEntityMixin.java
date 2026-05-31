9c7b5176f317adc3d5d115782363973283f12ef2
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-05-31 15:40:30 +0300

    Improved AbyssalAltarBlockEntityMixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexscaves/AbyssalAltarBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/alexscaves/AbyssalAltarBlockEntityMixin.java
@@ -1,20 +1,20 @@
 package net.therealvoin.notjustspoiled.mixin.mods.alexscaves;
 
+import com.github.alexmodguy.alexscaves.server.block.blockentity.AbyssalAltarBlockEntity;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Pseudo
-@Mixin(targets = "com.github.alexmodguy.alexscaves.server.block.blockentity.AbyssalAltarBlockEntity", remap = false)
-public class AbyssalAltarBlockEntityMixin {
-    @Inject(method = {"setItem", "m_6836_"}, at = @At("TAIL"))
-    private void changeFoodEnvironmentWhenPlacedOnAbyssalAltar(int index, ItemStack stackToPutOnAbyssalAltar, CallbackInfo ci) {
+@Mixin(AbyssalAltarBlockEntity.class)
+public abstract class AbyssalAltarBlockEntityMixin {
+    @Inject(method = "setItem", at = @At("TAIL"))
+    private void changeFoodEnvironmentWhenPlacedOnAbyssalAltar(CallbackInfo ci, @Local(argsOnly = true) ItemStack stackToPutOnAbyssalAltar) {
         FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPutOnAbyssalAltar, FoodEnvironment.GROUND, ((BlockEntity)(Object)this).getLevel());
     }
 }
