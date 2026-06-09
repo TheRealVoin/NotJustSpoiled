af5f25052de11bc86df26a78a164abc894fd82ac
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-09 12:53:20 +0300

    Fixed issue, where placing one piece of food updates the whole stack

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/farmersdelight/AbstractStoveBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/farmersdelight/AbstractStoveBlockEntityMixin.java
@@ -1,30 +1,30 @@
 package net.therealvoin.notjustspoiled.mixin.mods.farmersdelight;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.core.foodspoilage.FoodSpoilageManager;
 import net.therealvoin.notjustspoiled.util.NJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import vectorwing.farmersdelight.common.block.StoveBlock;
+import vectorwing.farmersdelight.common.block.AbstractStoveBlock;
 import vectorwing.farmersdelight.common.block.entity.AbstractStoveBlockEntity;
-import vectorwing.farmersdelight.common.block.entity.StoveBlockEntity;
 
 @Mixin(AbstractStoveBlockEntity.class)
 public class AbstractStoveBlockEntityMixin {
     @Inject(method = "cookAndOutputItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isItemEnabled(Lnet/minecraft/world/flag/FeatureFlagSet;)Z", shift = At.Shift.BEFORE))
-    private void copyCapToCookedFood(CallbackInfo ci, @Local(name = "ingredient") ItemStack ingredient, @Local(name = "result") ItemStack result) {
+    private void copySpoilageCapToCookedFood(CallbackInfo ci, @Local(name = "ingredient") ItemStack ingredient, @Local(name = "result") ItemStack result) {
         NJSUtils.copyCapability(ingredient, result, ((AbstractStoveBlockEntity)(Object)this).getLevel());
     }
 
-    @Inject(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemStackHandler;setStackInSlot(ILnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.BEFORE), remap = false)
-    private void updateFoodWhenPlacedOnStove(Entity entity, ItemStack foodStackToPlace, int foodCookingTime, CallbackInfoReturnable<Boolean> cir) {
-        FoodEnvironment foodEnvironment = ((StoveBlockEntity)(Object)this).getBlockState().getValue(StoveBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.GROUND;
-        FoodSpoilageManager.changeEnvironmentAndUpdate(foodStackToPlace, foodEnvironment, ((AbstractStoveBlockEntity)(Object)this).getLevel());
+    @ModifyArg(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemStackHandler;setStackInSlot(ILnet/minecraft/world/item/ItemStack;)V"), index = 1, remap = false)
+    private ItemStack updateFoodWhenPlacedOnStove(ItemStack stackToPlaceOnStove) {
+        FoodEnvironment foodEnvironment = ((AbstractStoveBlockEntity)(Object)this).getBlockState().getValue(AbstractStoveBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.GROUND;
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnStove, foodEnvironment, ((BlockEntity)(Object)this).getLevel());
+        return stackToPlaceOnStove;
     }
 }
