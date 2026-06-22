a526e19e73a992cc7a660484dd7902495eb59a24
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-22 13:22:07 +0300

    Moved away from forge capabilities to SavedData

--- src/main/java/net/therealvoin/notjustspoiled/mixin/mods/farmersdelight/FeastBlockMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/farmersdelight/FeastBlockMixin.java
@@ -2,10 +2,11 @@ package net.therealvoin.notjustspoiled.mixin.mods.farmersdelight;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.therealvoin.notjustspoiled.common.foodspoilage.capability.blockfoodspoilage.BlockFoodSpoilageProvider;
+import net.therealvoin.notjustspoiled.common.foodspoilage.BlockFoodSpoilage;
 import net.therealvoin.notjustspoiled.common.util.NJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,8 +18,9 @@ import vectorwing.farmersdelight.common.block.FeastBlock;
 public class FeastBlockMixin {
     @Inject(method = "takeServing", at = @At(value = "INVOKE_ASSIGN", target = "Lvectorwing/farmersdelight/common/block/FeastBlock;getServingItem(Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER))
     private void copySpoilageCapToFood(CallbackInfoReturnable<InteractionResult> cir, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockPos blockPos, @Local(name = "serving") ItemStack serving) {
-        level.getCapability(BlockFoodSpoilageProvider.BLOCK_FOOD_SPOILAGE).ifPresent(blockFoodSpoilage -> {
-            NJSUtils.copyCapability(blockFoodSpoilage.getLastItemStackByPos(blockPos), serving, level);
-        });
+        if (level instanceof ServerLevel serverLevel) {
+            BlockFoodSpoilage data = BlockFoodSpoilage.get(serverLevel);
+            NJSUtils.copyCapability(data.getLastItemStackByPos(blockPos), serving, level);
+        }
     }
 }
