ec9f53264cf2b19e9b6bebed04b7f7ddb84d9742
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-25 08:43:52 +0300

    Added new food crafting modes - FRESH_STATUS and FRESH_OR_STALE_STATUS

--- src/main/java/net/therealvoin/notjustspoiled/common/config/FoodCraftingMode.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/config/FoodCraftingMode.java
@@ -3,5 +3,7 @@ package net.therealvoin.notjustspoiled.common.config;
 public enum FoodCraftingMode {
     AVERAGE,
     SAME_STATUS,
-    WORST_STATUS
+    WORST_STATUS,
+    FRESH_STATUS,
+    FRESH_OR_STALE_STATUS
 }
