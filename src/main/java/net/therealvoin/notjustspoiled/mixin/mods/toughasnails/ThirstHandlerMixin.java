64a6d7418d6321909966b37a6d9356e71e50fbd5
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-25 08:56:27 +0300

    Added special integration with Tough As Nails

---
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/mods/toughasnails/ThirstHandlerMixin.java
@@ -0,0 +1,44 @@
+package net.therealvoin.notjustspoiled.mixin.mods.toughasnails;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilage;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodStatus;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import toughasnails.api.potion.TANEffects;
+import toughasnails.thirst.ThirstHandler;
+
+@Mixin(value = ThirstHandler.class, remap = false)
+public abstract class ThirstHandlerMixin {
+    @ModifyArgs(method = "onItemUseFinish", at = @At(value = "INVOKE", target = "Ltoughasnails/api/thirst/IThirst;drink(IF)V"))
+    private static void modifyThirstValuesAndApplyEffects(Args args, @Local(name = "player") Player player, @Local(name = "drink") ItemStack itemStack) {
+        if (player.level() instanceof ServerLevel serverLevel) {
+            FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(itemStack, serverLevel);
+            if (foodStatus == null) {
+                return;
+            }
+
+            FoodSpoilageManager.updateFoodLifetime(FoodSpoilage.of(itemStack), serverLevel);
+            args.set(0, foodStatus.getModifiedNutrition(args.get(0)));
+            args.set(1, foodStatus.getModifiedSaturation(args.get(1)));
+
+            RandomSource random = serverLevel.getRandom();
+            for (FoodStatus.EffectData effectData : foodStatus.getEffectsData()) {
+                MobEffect effect = effectData.getEffect() == MobEffects.HUNGER ? TANEffects.THIRST : effectData.getEffect();
+
+                if (random.nextDouble() < effectData.getApplyChance()) {
+                    player.addEffect(effectData.createEffectInstance(effect), player);
+                }
+            }
+        }
+    }
+}
