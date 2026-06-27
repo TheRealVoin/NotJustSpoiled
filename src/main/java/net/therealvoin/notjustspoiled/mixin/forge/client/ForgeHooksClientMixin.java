bd3dd03f0bf1e4aacb17498ee4c74ed33b0dc72f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-27 15:19:28 +0300

    Moved away from forge capabilities

--- src/main/java/net/therealvoin/notjustspoiled/mixin/forge/client/ForgeHooksClientMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/forge/client/ForgeHooksClientMixin.java
@@ -2,7 +2,7 @@ package net.therealvoin.notjustspoiled.mixin.forge.client;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.ForgeHooksClient;
-import net.therealvoin.notjustspoiled.common.foodspoilage.capability.FoodSpoilageProvider;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ForgeHooksClientMixin {
     @Inject(method = "shouldCauseReequipAnimation", at = @At("RETURN"), cancellable = true)
     private static void cancelRedundantReequipAnimationWhenCapabilityChanged(ItemStack from, ItemStack to, int slot, CallbackInfoReturnable<Boolean> cir) {
-        from.getCapability(FoodSpoilageProvider.FOOD_SPOILAGE).ifPresent(foodSpoilage1 -> {
-            to.getCapability(FoodSpoilageProvider.FOOD_SPOILAGE).ifPresent(foodSpoilage2 -> {
-                cir.setReturnValue(false);
-            });
-        });
+        FoodSpoilage foodSpoilage1 = FoodSpoilage.of(from);
+        FoodSpoilage foodSpoilage2 = FoodSpoilage.of(to);
+
+        if (foodSpoilage1 != null && foodSpoilage2 != null) {
+            cir.setReturnValue(false);
+        }
     }
 }
