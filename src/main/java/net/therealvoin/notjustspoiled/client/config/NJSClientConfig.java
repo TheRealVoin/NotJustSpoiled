157c6283546c3309e1a69957d9d09e777e1deff2
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-27 15:17:30 +0300

    Added configs for advanced tooltip information

--- src/main/java/net/therealvoin/notjustspoiled/client/config/NJSClientConfig.java
+++ src/main/java/net/therealvoin/notjustspoiled/client/config/NJSClientConfig.java
@@ -9,6 +9,8 @@ public class NJSClientConfig {
     public static final ForgeConfigSpec.BooleanValue SHOW_CATEGORY_IN_TOOLTIP;
     public static final ForgeConfigSpec.BooleanValue SHOW_ADDITIONAL_INFO;
     public static final ForgeConfigSpec.BooleanValue FOOD_OVERLAY;
+    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS;
+    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS_TO_BEING_SPOILED;
 
     static {
         BUILDER = new ForgeConfigSpec.Builder();
@@ -22,14 +24,24 @@ public class NJSClientConfig {
 
         BUILDER.translation("config.notjustspoiled.category.tooltip").push("Tooltip");
         SHOW_CATEGORY_IN_TOOLTIP = BUILDER
-                .comment("Shows food category in tooltip. If food doesn't have category, then \"null\" will be displayed.")
+                .comment("Shows food category in tooltip. If food doesn't have category, then nothing will be displayed.")
                 .translation("config.notjustspoiled.tooltip.show_food_category")
                 .define("showCategoryInTooltip", false);
 
         FOOD_OVERLAY = BUILDER
-                .comment("h")
+                .comment("If enabled, the slot containing the food will glow in a color matching to its status")
                 .translation("config.notjustspoiled.tooltip.food_overlay")
                 .define("foodOverlay", true);
+
+        SHOW_REMAINING_DAYS = BUILDER
+                .comment("Shows the number of game days until the food changes to the next status")
+                .translation("config.notjustspoiled.tooltip.show_remaining_days")
+                .define("showRemainingDays", true);
+
+        SHOW_REMAINING_DAYS_TO_BEING_SPOILED = BUILDER
+                .comment("Shows the number of game days until the food becomes spoiled")
+                .translation("config.notjustspoiled.tooltip.show_remaining_days_to_being_spoiled")
+                .define("showRemainingDaysToBeingSpoiled", false);
         BUILDER.pop();
 
         CLIENT_CONFIG = BUILDER.build();
