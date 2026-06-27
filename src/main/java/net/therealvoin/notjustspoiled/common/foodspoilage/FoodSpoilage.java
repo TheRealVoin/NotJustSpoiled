0ff78a8606889e1c2ec0e11fd20282a21ad6c738
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-27 15:19:33 +0300

    Moved away from forge capabilities

---
+++ src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodSpoilage.java
@@ -0,0 +1,48 @@
+package net.therealvoin.notjustspoiled.common.foodspoilage;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+
+public class FoodSpoilage {
+    private final ItemStack itemStack;
+
+    private FoodSpoilage(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public long getLastUpdateTime() {
+        return this.getFoodSpoilageTag().contains("lastUpdateTime") ? this.getFoodSpoilageTag().getLong("lastUpdateTime") : -1;
+    }
+
+    public void setLastUpdateTime(long lastUpdateTime) {
+        this.getFoodSpoilageTag().putLong("lastUpdateTime", lastUpdateTime);
+    }
+
+    public double getFoodLifetime() {
+        return this.getFoodSpoilageTag().contains("foodLifetime") ? this.getFoodSpoilageTag().getDouble("foodLifetime") : 0;
+    }
+
+    public void setFoodLifetime(double foodLifetime) {
+        this.getFoodSpoilageTag().putDouble("foodLifetime", foodLifetime);
+    }
+
+    public FoodEnvironment getEnvironment() {
+        return this.getFoodSpoilageTag().contains("environment") ? FoodEnvironment.valueOf(this.getFoodSpoilageTag().getString("environment")) : FoodEnvironment.NONE;
+    }
+
+    public void setEnvironment(FoodEnvironment foodEnvironment) {
+        this.getFoodSpoilageTag().putString("environment", foodEnvironment.name());
+    }
+
+    private CompoundTag getFoodSpoilageTag() {
+        return this.itemStack.getOrCreateTagElement("food_spoilage");
+    }
+
+    public static FoodSpoilage of(ItemStack itemStack) {
+        if (FoodCategory.getFoodCategory(itemStack) == null) {
+            return null;
+        }
+
+        return new FoodSpoilage(itemStack);
+    }
+}
