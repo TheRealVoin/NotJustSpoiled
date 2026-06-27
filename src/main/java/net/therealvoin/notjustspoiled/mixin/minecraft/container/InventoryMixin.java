7519d6694ebd35cc2fa807b5656b917b38a818fc
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-06-27 15:24:16 +0300

    Improved code

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/InventoryMixin.java
@@ -2,7 +2,6 @@ package net.therealvoin.notjustspoiled.mixin.minecraft.container;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
@@ -12,14 +11,57 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Inventory.class)
 public abstract class InventoryMixin {
     @Shadow @Final public Player player;
 
-    @Inject(method = "addResource(ILnet/minecraft/world/item/ItemStack;)I", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/player/Inventory;getItem(I)Lnet/minecraft/world/item/ItemStack;"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void updateFood(int pSlot, ItemStack pStack, CallbackInfoReturnable<Integer> cir, Item item, int i, ItemStack itemStack) {
+    @Inject(method = "addResource(ILnet/minecraft/world/item/ItemStack;)I", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/player/Inventory;getItem(I)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER))
+    private void changeFoodEnvironmentWhenPlacedInInventory(int pSlot, ItemStack pStack, CallbackInfoReturnable<Integer> cir) {
         FoodSpoilageManager.changeEnvironmentAndUpdate(pStack, FoodEnvironment.INVENTORY, player.level());
     }
+
+//    @WrapOperation(method = "addResource(ILnet/minecraft/world/item/ItemStack;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
+//    private void updateFood(ItemStack instance, int increment, Operation<Void> original, @Local(argsOnly = true) ItemStack pStack) {
+//        Level level = player.level();
+//
+//        if (instance.isEmpty()) {
+//            original.call(instance, increment);
+//            FoodSpoilageManager.changeEnvironmentAndUpdate(instance, FoodEnvironment.INVENTORY, level);
+//        } else {
+//            FoodSpoilageManager.changeEnvironmentAndUpdate(pStack, FoodEnvironment.INVENTORY, level);
+//            FoodSpoilageManager.tryAverageSpoilageOnMerge(instance, pStack, level);
+//            original.call(instance, increment);
+//        }
+//    }
+
+//    @ModifyArgs(method = "hasRemainingSpaceForItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+//    private void s(Args args) {
+//        ItemStack itemStack1 = args.get(0);
+//        ItemStack itemStack2 = args.get(1);
+//
+//        if (!FoodSpoilageManager.isInitialized(itemStack1) || !FoodSpoilageManager.isInitialized(itemStack2)) {
+//            return;
+//        }
+//
+//        if (!itemStack1.hasTag() || !itemStack1.getTag().contains("food_spoilage") || !itemStack2.hasTag() || !itemStack2.getTag().contains("food_spoilage")) {
+//            return;
+//        }
+//
+//        Level level = player.level();
+//        FoodStatus foodStatus1 = FoodSpoilageManager.getFoodStatus(itemStack1, level);
+//        FoodStatus foodStatus2 = FoodSpoilageManager.getFoodStatus(itemStack2, level);
+//
+//        if (foodStatus1 != foodStatus2) {
+//            return;
+//        }
+//
+//        ItemStack stackWithoutSpoilageTag1 = itemStack1.copy();
+//        ItemStack stackWithoutSpoilageTag2 = itemStack2.copy();
+//        stackWithoutSpoilageTag1.getTag().remove("food_spoilage");
+//        stackWithoutSpoilageTag2.getTag().remove("food_spoilage");
+//
+//        args.set(0, stackWithoutSpoilageTag1);
+//        args.set(1, stackWithoutSpoilageTag2);
+//    }
 }
