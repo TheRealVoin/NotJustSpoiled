73f17d0d7387617ef01030924d792c57d6174478
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-02 12:18:35 +0300

    Improved client config categories and comments

--- src/main/java/net/therealvoin/notjustspoiled/client/config/NJSClientConfig.java
+++ src/main/java/net/therealvoin/notjustspoiled/client/config/NJSClientConfig.java
@@ -3,47 +3,47 @@ package net.therealvoin.notjustspoiled.client.config;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class NJSClientConfig {
-    public static final ForgeConfigSpec CLIENT_CONFIG;
-    public static final ForgeConfigSpec.Builder BUILDER;
-
-    public static final ForgeConfigSpec.BooleanValue SHOW_CATEGORY_IN_TOOLTIP;
-    public static final ForgeConfigSpec.BooleanValue SHOW_ADDITIONAL_INFO;
-    public static final ForgeConfigSpec.BooleanValue FOOD_OVERLAY;
-    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS;
-    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS_TO_BEING_SPOILED;
+    public static final ForgeConfigSpec CONFIG;
+    public static final ForgeConfigSpec.BooleanValue FOOD_SLOT_OVERLAY;
+    public static final ForgeConfigSpec.BooleanValue SHOW_FOOD_CATEGORY;
+    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS_TO_NEXT_STATUS;
+    public static final ForgeConfigSpec.BooleanValue SHOW_REMAINING_DAYS_TO_SPOILED_STATUS;
+    public static final ForgeConfigSpec.BooleanValue SHOW_DEBUG_INFO;
 
     static {
-        BUILDER = new ForgeConfigSpec.Builder();
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
-        BUILDER.translation("config.notjustspoiled.category.debug").push("Debug");
-        SHOW_ADDITIONAL_INFO = BUILDER
-                .comment("Shows additional food info in tooltip (\"lastUpdateTime\", \"foodLifeTime\" and \"environment\").")
-                .translation("config.notjustspoiled.debug.show_additional_info")
-                .define("showAdditionalInfo", false);
-        BUILDER.pop();
-
-        BUILDER.translation("config.notjustspoiled.category.tooltip").push("Tooltip");
-        SHOW_CATEGORY_IN_TOOLTIP = BUILDER
-                .comment("Shows food category in tooltip. If food doesn't have category, then nothing will be displayed.")
+        builder.translation("config.notjustspoiled.tooltip").push("Tooltip");
+        SHOW_FOOD_CATEGORY = builder
+                .comment("Shows the food category in the tooltip if present.")
                 .translation("config.notjustspoiled.tooltip.show_food_category")
-                .define("showCategoryInTooltip", false);
+                .define("showFoodCategory", false);
 
-        FOOD_OVERLAY = BUILDER
-                .comment("If enabled, the slot containing the food will glow in a color matching to its status")
-                .translation("config.notjustspoiled.tooltip.food_overlay")
-                .define("foodOverlay", true);
+        SHOW_REMAINING_DAYS_TO_NEXT_STATUS = builder
+                .comment("Shows the number of in-game days until the food reaches the next status.")
+                .translation("config.notjustspoiled.tooltip.show_remaining_days_to_next_status")
+                .define("showRemainingDaysToNextStatus", true);
 
-        SHOW_REMAINING_DAYS = BUILDER
-                .comment("Shows the number of game days until the food changes to the next status")
-                .translation("config.notjustspoiled.tooltip.show_remaining_days")
-                .define("showRemainingDays", true);
+        SHOW_REMAINING_DAYS_TO_SPOILED_STATUS = builder
+                .comment("Shows the number of in-game days until the food becomes spoiled.")
+                .translation("config.notjustspoiled.tooltip.show_remaining_days_to_spoiled_status")
+                .define("showRemainingDaysToSpoiledStatus", false);
+        builder.pop();
 
-        SHOW_REMAINING_DAYS_TO_BEING_SPOILED = BUILDER
-                .comment("Shows the number of game days until the food becomes spoiled")
-                .translation("config.notjustspoiled.tooltip.show_remaining_days_to_being_spoiled")
-                .define("showRemainingDaysToBeingSpoiled", false);
-        BUILDER.pop();
+        builder.translation("config.notjustspoiled.visuals").push("Visuals");
+        FOOD_SLOT_OVERLAY = builder
+                .comment("Makes the food slot glow with a color corresponding to the food's status.")
+                .translation("config.notjustspoiled.visuals.food_slot_overlay")
+                .define("foodSlotOverlay", true);
+        builder.pop();
 
-        CLIENT_CONFIG = BUILDER.build();
+        builder.translation("config.notjustspoiled.debug").push("Debug");
+        SHOW_DEBUG_INFO = builder
+                .comment("Shows additional debug information in the tooltip.")
+                .translation("config.notjustspoiled.debug.show_debug_info")
+                .define("showDebugInfo", false);
+        builder.pop();
+
+        CONFIG = builder.build();
     }
 }
