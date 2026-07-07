317b8a37218de3ef5a1629e810185e30c7770b60
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-07 16:20:56 +0300

    Removed NONE food environment

--- src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodEnvironment.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodEnvironment.java
@@ -4,7 +4,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.therealvoin.notjustspoiled.common.config.NJSServerConfig;
 
 public enum FoodEnvironment {
-    NONE(null),
     INVENTORY(NJSServerConfig.FOOD_SPOILAGE_IN_INVENTORY_MULTIPLIER),
     STORAGE(NJSServerConfig.FOOD_SPOILAGE_IN_STORAGE_MULTIPLIER),
     OPEN_AIR(NJSServerConfig.FOOD_SPOILAGE_ON_OPEN_AIR_MULTIPLIER),
