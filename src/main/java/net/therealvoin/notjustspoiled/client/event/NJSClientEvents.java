dddda45e5b1025effc8a4daf34099c17cea0057d
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-08 14:04:51 +0300

    Various code improvements

--- src/main/java/net/therealvoin/notjustspoiled/client/event/NJSClientEvents.java
+++ src/main/java/net/therealvoin/notjustspoiled/client/event/NJSClientEvents.java
@@ -2,7 +2,10 @@ package net.therealvoin.notjustspoiled.client.event;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -25,15 +28,17 @@ import java.util.List;
 public class NJSClientEvents {
     @Mod.EventBusSubscriber(modid = NotJustSpoiled.MOD_ID, value = Dist.CLIENT)
     public static class ForgeBus {
-        private static final Component STATUS = Component.translatable("tooltip.notjustspoiled.status").append(": ").withStyle(ChatFormatting.GRAY);
+        private static final Component CATEGORY = Component.translatable("tooltip.notjustspoiled.food_category").append(": ").withStyle(ChatFormatting.GRAY);
+        private static final Component STATUS = Component.translatable("tooltip.notjustspoiled.food_status").append(": ").withStyle(ChatFormatting.GRAY);
+        private static final Component NEVER_SPOILS = Component.translatable("tooltip.notjustspoiled.never_spoils").withStyle(ChatFormatting.AQUA);
         private static final Component TO_NEXT_STATUS = Component.translatable("tooltip.notjustspoiled.to_next_status").append(": ").withStyle(ChatFormatting.GRAY);
         private static final Component SPOILS_IN = Component.translatable("tooltip.notjustspoiled.spoils_in").append(": ").withStyle(ChatFormatting.GRAY);
-        private static final Component NEVER_SPOILS = Component.translatable("tooltip.notjustspoiled.never_spoils").withStyle(ChatFormatting.AQUA);
-        private static final Component CATEGORY = Component.translatable("tooltip.notjustspoiled.debug.category").append(": ").withStyle(ChatFormatting.GRAY);
 
         @SubscribeEvent
-        public static void addFoodStatusToTooltip(ItemTooltipEvent event) {
-            if (event.getEntity() == null) {
+        public static void addFoodSpoilageInfoToTooltip(ItemTooltipEvent event) {
+            Player player = event.getEntity();
+
+            if (player == null) {
                 return;
             }
 
@@ -48,15 +53,14 @@ public class NJSClientEvents {
                 return;
             }
 
-            FoodCategory foodCategory = FoodCategory.getFoodCategory(tooltipItem);
-            if (NJSClientConfig.SHOW_CATEGORY_IN_TOOLTIP.get()) {
-                if (foodCategory != null) {
-                    Component category = Component.literal(String.valueOf(foodCategory)).withStyle(ChatFormatting.LIGHT_PURPLE);
-                    tooltip.add(CATEGORY.copy().append(category));
+            FoodCategory tooltipItemCategory = FoodCategory.getFoodCategory(tooltipItem);
+            Level level = player.level();
+
+            if (NJSClientConfig.SHOW_FOOD_CATEGORY.get()) {
+                if (tooltipItemCategory != null) {
+                    tooltip.add(CATEGORY.copy().append(tooltipItemCategory.getTranslation()));
                 }
             }
-
-            Level level = event.getEntity().level();
 
             FoodStatus currentFoodStatus = FoodSpoilageManager.getFoodStatus(tooltipItem, level);
             if (currentFoodStatus == null) {
@@ -65,86 +69,80 @@ public class NJSClientEvents {
 
             tooltip.add(STATUS.copy().append(currentFoodStatus.getTranslation()));
 
-            boolean showNextStatus = false;
-            boolean showSpoilsIn = false;
+            FoodSpoilage foodSpoilage = FoodSpoilage.of(tooltipItem);
+            FoodStatus nextFoodStatus = currentFoodStatus.getNext();
 
             if (currentFoodStatus != FoodStatus.SPOILED) {
-                if (currentFoodStatus == FoodStatus.HALF_SPOILED) {
-                    showSpoilsIn = NJSClientConfig.SHOW_REMAINING_DAYS.get() || NJSClientConfig.SHOW_REMAINING_DAYS_TO_BEING_SPOILED.get();
-                } else {
-                    showNextStatus = NJSClientConfig.SHOW_REMAINING_DAYS.get();
-                    showSpoilsIn = NJSClientConfig.SHOW_REMAINING_DAYS_TO_BEING_SPOILED.get();
+                if (NJSClientConfig.SHOW_REMAINING_DAYS_TO_NEXT_FOOD_STATUS.get() && nextFoodStatus != FoodStatus.SPOILED) {
+                    tooltip.add((createTranslationLine(TO_NEXT_STATUS, currentFoodStatus.getThreshold(tooltipItemCategory.getSpoilageTime()), foodSpoilage, level)));
+                }
+
+                if (NJSClientConfig.SHOW_REMAINING_DAYS_TO_SPOILED_STATUS.get() || NJSClientConfig.SHOW_REMAINING_DAYS_TO_NEXT_FOOD_STATUS.get() && nextFoodStatus == FoodStatus.SPOILED) {
+                    tooltip.add(createTranslationLine(SPOILS_IN, tooltipItemCategory.getSpoilageTime(), foodSpoilage, level));
                 }
             }
 
-            if (showNextStatus) {
-                FoodStatus next = currentFoodStatus.getNext();
-                if (next != null) {
-                    double remainingTime = currentFoodStatus.getThreshold(foodCategory.getSpoilageTime()) - FoodSpoilageManager.calculateActualFoodLifetime(FoodSpoilage.of(tooltipItem), level);
-                    int totalTicks = (int) Math.floor(remainingTime / FoodSpoilage.of(tooltipItem).getEnvironment().getFoodSpoilageMultiplier());
-                    int days = totalTicks / 24000;
-
-                    if (days > 0) {
-                        tooltip.add(TO_NEXT_STATUS.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.days", days).withStyle(ChatFormatting.WHITE)));
-                    } else {
-                        tooltip.add(TO_NEXT_STATUS.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.one_day").withStyle(ChatFormatting.WHITE)));
-                    }
-                }
-            }
-
-            if (showSpoilsIn) {
-                double remainingTicks = foodCategory.getSpoilageTime() - FoodSpoilageManager.calculateActualFoodLifetime(FoodSpoilage.of(tooltipItem), level);
-                int days = (int) Math.floor(remainingTicks / FoodSpoilage.of(tooltipItem).getEnvironment().getFoodSpoilageMultiplier() / 24000);
-
-                if (days > 0) {
-                    tooltip.add(SPOILS_IN.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.days", days).withStyle(ChatFormatting.WHITE)));
-                } else {
-                    tooltip.add(SPOILS_IN.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.one_day", days).withStyle(ChatFormatting.WHITE)));
-                }
-            }
-
-            if (NJSClientConfig.SHOW_ADDITIONAL_INFO.get()) {
-                FoodSpoilage foodSpoilage = FoodSpoilage.of(tooltipItem);
+            if (NJSClientConfig.SHOW_DEBUG_INFO.get()) {
                 tooltip.add(Component.literal("lastUpdateTime: ").append(String.valueOf(foodSpoilage.getLastUpdateTime())));
                 tooltip.add(Component.literal("foodLifetime: ").append(String.valueOf(foodSpoilage.getFoodLifetime())));
                 tooltip.add(Component.literal("environment: ").append(foodSpoilage.getEnvironment().name()));
             }
         }
+
+        private static Component createTranslationLine(Component component, double someNumber, FoodSpoilage foodSpoilage, Level level) {
+            int days = (int) Math.floor((someNumber - FoodSpoilageManager.calculateActualFoodLifetime(foodSpoilage, level)) / foodSpoilage.getEnvironment().getFoodSpoilageMultiplier()) / 24000;
+
+            if (days > 0) {
+                return component.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.days", days));
+            }
+
+            return component.copy().append(Component.translatable("tooltip.notjustspoiled.to_next_status.one_day"));
+        }
     }
 
     @Mod.EventBusSubscriber(modid = NotJustSpoiled.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ModBus {
+        private static final int OVERLAY_ALPHA = 0x80000000;
+
         @SubscribeEvent
-        public static void changeSlotColorBasedOnFoodStatus(RegisterItemDecorationsEvent event) {
+        public static void registerFoodSlotOverlay(RegisterItemDecorationsEvent event) {
             for (Item item : ForgeRegistries.ITEMS) {
-                event.register(item, (guiGraphics, font, stack, xOffset, yOffset) -> {
-                    if (NJSClientConfig.FOOD_OVERLAY.get()) {
-                        if (stack.is(NJSTags.Items.ALWAYS_SPOILED)) {
-                            guiGraphics.fill(xOffset, yOffset, xOffset + 16, yOffset + 16, 0x80000000 | ChatFormatting.RED.getColor());
-                            return true;
-                        } else if (stack.is(NJSTags.Items.NEVER_SPOILS)) {
-                            guiGraphics.fill(xOffset, yOffset, xOffset + 16, yOffset + 16, 0x80000000 | ChatFormatting.AQUA.getColor());
-                            return true;
-                        }
-
-                        FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(stack, Minecraft.getInstance().level);
-                        if (foodStatus == null) {
-                            return false;
-                        }
-
-                        ItemStack carried = Minecraft.getInstance().player.containerMenu.getCarried();
-                        if (carried == stack) {
-                            return false;
-                        }
-
-                        int color = 0x80000000 | foodStatus.getColor().getColor();
-                        guiGraphics.fill(xOffset, yOffset, xOffset + 16, yOffset + 16, color);
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
+                event.register(item, ModBus::renderFoodSlotOverlay);
             }
+        }
+
+        private static boolean renderFoodSlotOverlay(GuiGraphics guiGraphics, Font font, ItemStack stack, int xOffset, int yOffset) {
+            if (!NJSClientConfig.FOOD_SLOT_OVERLAY.get()) {
+                return false;
+            }
+
+            Minecraft minecraft = Minecraft.getInstance();
+
+            // We don't need to render overlay if item is carried by player as it looks weird
+            ItemStack carriedStack = minecraft.player.containerMenu.getCarried();
+            if (carriedStack == stack) {
+                return false;
+            }
+
+            if (stack.is(NJSTags.Items.ALWAYS_SPOILED)) {
+                drawOverlay(guiGraphics, xOffset, yOffset, FoodStatus.SPOILED.getColor());
+                return true;
+            } else if (stack.is(NJSTags.Items.NEVER_SPOILS)) {
+                drawOverlay(guiGraphics, xOffset, yOffset, ChatFormatting.AQUA);
+                return true;
+            }
+
+            FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(stack, minecraft.level);
+            if (foodStatus == null) {
+                return false;
+            }
+
+            drawOverlay(guiGraphics, xOffset, yOffset, foodStatus.getColor());
+            return true;
+        }
+
+        private static void drawOverlay(GuiGraphics guiGraphics, int xOffset, int yOffset, ChatFormatting chatFormatting) {
+            guiGraphics.fill(xOffset, yOffset, xOffset + 16, yOffset + 16, OVERLAY_ALPHA | chatFormatting.getColor());
         }
     }
 }
