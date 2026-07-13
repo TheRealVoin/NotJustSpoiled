f6fad0149d8cbc7c1c1121860da39e3a4529d51d
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-13 20:21:22 +0300

    Added logger, cosmetic changes

--- src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
+++ src/main/java/net/therealvoin/notjustspoiled/NotJustSpoiled.java
@@ -1,5 +1,6 @@
 package net.therealvoin.notjustspoiled;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -9,15 +10,18 @@ import net.therealvoin.notjustspoiled.common.config.NJSServerConfig;
 import net.therealvoin.notjustspoiled.common.init.NJSGlobalLootModifiers;
 import net.therealvoin.notjustspoiled.common.network.NJSNetwork;
 import net.therealvoin.notjustspoiled.compat.appleskin.AppleSkinCompat;
+import org.slf4j.Logger;
 
 @Mod(NotJustSpoiled.MOD_ID)
 public class NotJustSpoiled {
-    public static final String MOD_ID = "notjustspoiled";
     public static final String MOD_NAME = "Not Just Spoiled";
+    public static final String MOD_ID = "notjustspoiled";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public NotJustSpoiled(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
-        NJSGlobalLootModifiers.GLOBAL_LOOT_MODIFIERS_DEFERRED_REGISTER.register(modEventBus);
+
+        NJSGlobalLootModifiers.GLOBAL_LOOT_MODIFIERS.register(modEventBus);
         context.registerConfig(ModConfig.Type.CLIENT, NJSClientConfig.CONFIG);
         context.registerConfig(ModConfig.Type.SERVER, NJSServerConfig.CONFIG);
         NJSNetwork.register();
