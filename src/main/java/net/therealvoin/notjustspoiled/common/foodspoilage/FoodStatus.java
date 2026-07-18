00a9de6c04566b08e041affc072b30e01c7c9c1e
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-18 19:49:29 +0300

    Huge code improvements

--- src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodStatus.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodStatus.java
@@ -1,32 +1,26 @@
 package net.therealvoin.notjustspoiled.common.foodspoilage;
 
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.therealvoin.notjustspoiled.common.config.NJSServerConfig;
+import net.therealvoin.notjustspoiled.common.data.foodstatus.FoodStatusEffect;
 import net.therealvoin.notjustspoiled.common.data.foodstatus.FoodStatusReloadListener;
 
 public enum FoodStatus {
-    FRESH("fresh", ChatFormatting.GREEN, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE),
-    STALE("stale", ChatFormatting.YELLOW, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_STALE_FOOD_IN_STORAGE),
-    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_HALF_SPOILED_FOOD_IN_STORAGE),
-    SPOILED("spoiled", ChatFormatting.RED, NJSServerConfig.RANDOM$CHANCE_TO_APPEAR_SPOILED_FOOD_IN_STORAGE);
+    FRESH("fresh", ChatFormatting.GREEN, 0),
+    STALE("stale", ChatFormatting.YELLOW, 1.0 / 3.0),
+    HALF_SPOILED("half-spoiled", ChatFormatting.GOLD, 2.0 / 3.0),
+    SPOILED("spoiled", ChatFormatting.RED, 1);
 
     private final String key;
     private final ChatFormatting color;
-    private final ForgeConfigSpec.DoubleValue chanceToAppearInStorage;
+    private final double start;
 
-    FoodStatus(String key, ChatFormatting color, ForgeConfigSpec.DoubleValue chanceToAppearInStorage) {
+    FoodStatus(String key, ChatFormatting color, double start) {
         this.key = key;
         this.color = color;
-        this.chanceToAppearInStorage = chanceToAppearInStorage;
+        this.start = start;
     }
 
     public Component getDisplayName() {
@@ -37,21 +31,38 @@ public enum FoodStatus {
         return this.color;
     }
 
-    public double getChanceToAppearInStorage() {
-        return this.chanceToAppearInStorage.get();
-    }
-
-    public double getThreshold(int spoilageTime) {
-        return switch (this) {
-            case FRESH -> spoilageTime / 3.0;
-            case STALE -> spoilageTime / 3.0 * 2;
-            case HALF_SPOILED -> spoilageTime;
-            case SPOILED -> throw new IllegalStateException("Spoiled status has no threshold");
-        };
-    }
-
     public FoodStatus getNext() {
         return this.ordinal() < values().length - 1 ? values()[this.ordinal() + 1] : null;
+    }
+
+    public double getStart(int spoilageTime) {
+        return spoilageTime * this.start;
+    }
+
+    public double getEnd(int spoilageTime) {
+        FoodStatus nextFoodStatus = this.getNext();
+        return nextFoodStatus == null ? spoilageTime : nextFoodStatus.getStart(spoilageTime) - 1;
+    }
+
+    public int getModifiedNutrition(int defaultNutrition) {
+        return Math.max(1, (int) Math.round(defaultNutrition * FoodStatusReloadListener.get(this).nutritionMultiplier()));
+    }
+
+    public void applyEffects(LivingEntity entity) {
+        for (FoodStatusEffect effectData : FoodStatusReloadListener.get(this).effects()) {
+            if (entity.getRandom().nextFloat() < effectData.applyChance()) {
+                entity.addEffect(
+                        new MobEffectInstance(
+                                effectData.effect(),
+                                effectData.duration(),
+                                effectData.amplifier(),
+                                false,
+                                false
+                        ),
+                        entity
+                );
+            }
+        }
     }
 
     public static FoodStatus byName(String name) {
@@ -62,155 +73,5 @@ public enum FoodStatus {
         }
 
         return null;
-    }
-
-    public int getModifiedNutrition(int defaultNutrition) {
-        switch (this) {
-            case STALE -> {
-                if (defaultNutrition <= 4) {
-                    return defaultNutrition;
-                } else if (defaultNutrition <= 10) {
-                    return defaultNutrition - 1;
-                } else if (defaultNutrition <= 14) {
-                    return defaultNutrition - 2;
-                } else if (defaultNutrition <= 22) {
-                    return defaultNutrition - 3;
-                } else {
-                    return defaultNutrition - 4;
-                }
-            }
-
-            case HALF_SPOILED -> {
-                if (defaultNutrition == 1) {
-                    return defaultNutrition;
-                } else if (defaultNutrition <= 3) {
-                    return defaultNutrition - 1;
-                } else if (defaultNutrition <= 6) {
-                    return defaultNutrition - 2;
-                } else if (defaultNutrition <= 8) {
-                    return defaultNutrition - 3;
-                } else if (defaultNutrition <= 10) {
-                    return defaultNutrition - 4;
-                } else if (defaultNutrition <= 12) {
-                    return defaultNutrition - 5;
-                } else if (defaultNutrition <= 14) {
-                    return defaultNutrition - 6;
-                } else if (defaultNutrition <= 16) {
-                    return defaultNutrition - 7;
-                } else if (defaultNutrition <= 18) {
-                    return defaultNutrition - 8;
-                } else if (defaultNutrition <= 20) {
-                    return defaultNutrition - 9;
-                } else if (defaultNutrition <= 22) {
-                    return defaultNutrition - 10;
-                } else {
-                    return defaultNutrition - 11;
-                }
-            }
-
-            case SPOILED -> {
-                if (defaultNutrition == 1) {
-                    return defaultNutrition;
-                } else if (defaultNutrition == 2) {
-                    return defaultNutrition - 1;
-                } else if (defaultNutrition == 3) {
-                    return defaultNutrition - 2;
-                } else if (defaultNutrition <= 5) {
-                    return defaultNutrition - 3;
-                } else if (defaultNutrition <= 7) {
-                    return defaultNutrition - 4;
-                } else if (defaultNutrition == 8) {
-                    return defaultNutrition - 5;
-                } else if (defaultNutrition == 9) {
-                    return defaultNutrition - 6;
-                } else if (defaultNutrition <= 11) {
-                    return defaultNutrition - 7;
-                } else if (defaultNutrition <= 13) {
-                    return defaultNutrition - 8;
-                } else if (defaultNutrition <= 15) {
-                    return defaultNutrition - 9;
-                } else if (defaultNutrition <= 17) {
-                    return defaultNutrition - 10;
-                } else if (defaultNutrition <= 22) {
-                    return defaultNutrition - 12;
-                } else {
-                    return defaultNutrition - 13;
-                }
-            }
-        }
-
-        return defaultNutrition;
-    }
-
-    public float getModifiedSaturation(float defaultSaturation) {
-        return defaultSaturation;
-    }
-
-    public static FoodStatus getFreshOrStaleStatus(RandomSource random) {
-        double randomDouble = random.nextDouble();
-
-        double fresh = NJSServerConfig.FRESH_OR_STALE$CHANCE_TO_APPEAR_FRESH_FOOD_IN_STORAGE.get();
-
-        if (randomDouble < fresh) {
-            return FRESH;
-        } else {
-            return STALE;
-        }
-    }
-
-    public static FoodStatus getRandomFoodStatus(RandomSource random) {
-        double randomDouble = random.nextDouble();
-
-        double fresh = FRESH.getChanceToAppearInStorage();
-        double stale = STALE.getChanceToAppearInStorage();
-        double halfSpoiled = HALF_SPOILED.getChanceToAppearInStorage();
-
-        if (randomDouble < fresh) {
-            return FRESH;
-        } else if (randomDouble < fresh + stale) {
-            return STALE;
-        } else if (randomDouble < fresh + stale + halfSpoiled) {
-            return HALF_SPOILED;
-        } else {
-            return SPOILED;
-        }
-    }
-
-    public void applyEffects(LivingEntity entity) {
-        RandomSource random = entity.getRandom();
-
-        for (FoodStatus.EffectData effectData : FoodStatusReloadListener.get(this).effects()) {
-            if (random.nextFloat() < effectData.applyChance()) {
-                entity.addEffect(effectData.createEffectInstance(), entity);
-            }
-        }
-    }
-
-    public record EffectData(MobEffect effect, int duration, int amplifier, float applyChance) {
-        public static final Codec<EffectData> CODEC = RecordCodecBuilder.create(
-                instance -> instance.group(
-                        ForgeRegistries.MOB_EFFECTS.getCodec()
-                                .fieldOf("effect")
-                                .forGetter(EffectData::effect),
-                        Codec.intRange(1, Integer.MAX_VALUE)
-                                .fieldOf("duration")
-                                .forGetter(EffectData::duration),
-                        Codec.intRange(0, 255)
-                                .fieldOf("amplifier")
-                                .forGetter(EffectData::amplifier),
-                        Codec.floatRange(0, 1)
-                                .fieldOf("chance")
-                                .forGetter(EffectData::applyChance)
-                ).apply(instance, EffectData::new)
-        );
-
-        public MobEffectInstance createEffectInstance() {
-            return createEffectInstance(this.effect);
-        }
-
-        // Added specifically for TAN integration
-        public MobEffectInstance createEffectInstance(MobEffect effect) {
-            return new MobEffectInstance(effect, this.duration, this.amplifier, false, false);
-        }
     }
 }
