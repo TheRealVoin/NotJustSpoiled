5e1f58740fe1414de625434d9d8ef7c0d7d2df88
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-18 18:04:59 +0300

    Replaced ModifyArgs with ModifyArg as it was redundant, code improvements

--- src/main/java/net/therealvoin/notjustspoiled/compat/mixin/toughasnails/ThirstHandlerMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/compat/mixin/toughasnails/ThirstHandlerMixin.java
@@ -2,43 +2,50 @@ package net.therealvoin.notjustspoiled.compat.mixin.toughasnails;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilage;
+import net.therealvoin.notjustspoiled.common.data.foodstatus.FoodStatusEffect;
+import net.therealvoin.notjustspoiled.common.data.foodstatus.FoodStatusReloadListener;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodStatus;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import toughasnails.api.potion.TANEffects;
 import toughasnails.thirst.ThirstHandler;
 
 @Mixin(value = ThirstHandler.class, remap = false)
 public abstract class ThirstHandlerMixin {
-    @ModifyArgs(method = "onItemUseFinish", at = @At(value = "INVOKE", target = "Ltoughasnails/api/thirst/IThirst;drink(IF)V"))
-    private static void modifyThirstValuesAndApplyEffects(Args args, @Local(name = "player") Player player, @Local(name = "drink") ItemStack itemStack) {
+    @ModifyArg(method = "onItemUseFinish", at = @At(value = "INVOKE", target = "Ltoughasnails/api/thirst/IThirst;drink(IF)V"), index = 0)
+    private static int modifyThirstValuesAndApplyEffects(int defaultThirst, @Local(name = "player") Player player, @Local(name = "drink") ItemStack itemStack) {
         if (player.level() instanceof ServerLevel serverLevel) {
             FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(itemStack, serverLevel);
             if (foodStatus == null) {
-                return;
+                return defaultThirst;
             }
 
             FoodSpoilageManager.updateFoodLifetime(itemStack, serverLevel);
-            args.set(0, foodStatus.getModifiedNutrition(args.get(0)));
-            args.set(1, foodStatus.getModifiedSaturation(args.get(1)));
 
-            RandomSource random = serverLevel.getRandom();
-            for (FoodStatus.EffectData effectData : foodStatus.getEffectsData()) {
-                MobEffect effect = effectData.effect() == MobEffects.HUNGER ? TANEffects.THIRST : effectData.effect();
-
-                if (random.nextDouble() < effectData.applyChance()) {
-                    player.addEffect(effectData.createEffectInstance(effect), player);
+            for (FoodStatusEffect effect : FoodStatusReloadListener.get(foodStatus).effects()) {
+                if (player.getRandom().nextFloat() < effect.applyChance()) {
+                    player.addEffect(
+                            new MobEffectInstance(
+                                    effect.effect() == MobEffects.HUNGER ? TANEffects.THIRST : effect.effect(),
+                                    effect.duration(),
+                                    effect.amplifier(),
+                                    false,
+                                    false
+                            ),
+                            player
+                    );
                 }
             }
+
+            return foodStatus.getModifiedNutrition(defaultThirst);
         }
+
+        return defaultThirst;
     }
 }
