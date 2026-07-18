63bef264fcc48f824f8134bcaa449de3b3ec75df
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-18 18:01:09 +0300

    Moved getLevelBySLot method to AbstractContainerMenuMixin

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/AbstractContainerMenuMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/container/AbstractContainerMenuMixin.java
@@ -1,32 +1,40 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.container;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.NonNullList;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.vehicle.AbstractMinecartContainer;
+import net.minecraft.world.entity.vehicle.ChestBoat;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilage;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
-import net.therealvoin.notjustspoiled.common.util.NJSUtils;
+import net.therealvoin.notjustspoiled.common.util.SimpleContainerAccessor;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractContainerMenu.class)
 public abstract class AbstractContainerMenuMixin {
     @Shadow @Final public NonNullList<Slot> slots;
     @Shadow private ItemStack carried;
 
-    @Inject(method = "moveItemStackTo", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void makeFoodPossibleForMerging(ItemStack stack, int startIndex, int endIndex, boolean reverseDirection, CallbackInfoReturnable<Boolean> cir, boolean flag, int i, Slot slot, ItemStack itemstack) {
-        Level level = NJSUtils.getLevelBySlot(slot);
+    @Inject(method = "moveItemStackTo", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z", shift = At.Shift.BEFORE))
+    private void makeFoodPossibleForMerging(CallbackInfoReturnable<Boolean> cir, @Local(argsOnly = true) ItemStack stack, @Local Slot slot, @Local(ordinal = 1) ItemStack itemstack) {
+        Level level = notJustSpoiled$getLevelBySlot(slot);
 
         if (level instanceof ServerLevel serverLevel) {
             FoodSpoilage foodSpoilage = FoodSpoilage.of(itemstack);
@@ -42,7 +50,7 @@ public abstract class AbstractContainerMenuMixin {
     private void changeFoodEnvironmentWhenPlacedInCarriedSlot(ItemStack itemStack, CallbackInfo ci) {
         Level level = null;
         for (Slot slot : this.slots) {
-            level = NJSUtils.getLevelBySlot(slot);
+            level = notJustSpoiled$getLevelBySlot(slot);
             if (level != null) {
                 break;
             }
@@ -58,7 +66,7 @@ public abstract class AbstractContainerMenuMixin {
     @Inject(method = "canItemQuickReplace", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
     private static void makeFoodPossibleForMerging(Slot slot, ItemStack stack, boolean stackSizeMatters, CallbackInfoReturnable<Boolean> cir) {
         ItemStack itemInSlot = slot.getItem();
-        Level level = NJSUtils.getLevelBySlot(slot);
+        Level level = notJustSpoiled$getLevelBySlot(slot);
 
         if (level instanceof ServerLevel serverLevel) {
             FoodSpoilage foodSpoilage = FoodSpoilage.of(itemInSlot);
@@ -67,5 +75,26 @@ public abstract class AbstractContainerMenuMixin {
                 FoodSpoilageManager.tryAverageSpoilageOnMerge(itemInSlot, stack, serverLevel);
             }
         }
+    }
+
+    @Unique
+    private static Level notJustSpoiled$getLevelBySlot(Slot slot) {
+        Container container = slot.container;
+        if (container instanceof Inventory inventory) {
+            return inventory.player.level();
+        } else if (container instanceof BaseContainerBlockEntity blockEntity) {
+            return blockEntity.getLevel();
+        } else if (container instanceof AbstractMinecartContainer minecart) {
+            return minecart.level();
+        } else if (container instanceof ChestBoat chestBoat) {
+            return chestBoat.level();
+        } else if (container instanceof SimpleContainer simpleContainer) {
+            AbstractHorse horse = ((SimpleContainerAccessor) simpleContainer).getHorse();
+            if (horse != null) {
+                return horse.level();
+            }
+        }
+
+        return null;
     }
 }
