f907083026f08046738e87fbc5daad971b8b541f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-18 19:50:56 +0300

    Replaced float with double

--- src/main/java/net/therealvoin/notjustspoiled/common/data/foodstatus/FoodStatusData.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/data/foodstatus/FoodStatusData.java
@@ -5,10 +5,10 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import java.util.List;
 
-public record FoodStatusData(float nutritionMultiplier, List<FoodStatusEffect> effects) {
+public record FoodStatusData(double nutritionMultiplier, List<FoodStatusEffect> effects) {
     public static final Codec<FoodStatusData> CODEC = RecordCodecBuilder.create(
             instance -> instance.group(
-                    Codec.floatRange(0, 1)
+                    Codec.doubleRange(0, 1)
                             .fieldOf("nutritionMultiplier")
                             .forGetter(FoodStatusData::nutritionMultiplier),
                     FoodStatusEffect.CODEC
