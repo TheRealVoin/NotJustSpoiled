3df57dc5a15d3a4e5f3304f31b071c7442bb3eb4
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-19 11:53:19 +0300

    Huge performance & code readability improvements

--- src/main/java/net/therealvoin/notjustspoiled/common/config/FoodCraftingMode.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/config/FoodCraftingMode.java
@@ -1,9 +1,73 @@
 package net.therealvoin.notjustspoiled.common.config;
 
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodStatus;
+
 public enum FoodCraftingMode {
-    AVERAGE,
-    SAME_STATUS,
-    WORST_STATUS,
-    FRESH_STATUS,
-    FRESH_OR_STALE_STATUS
+    AVERAGE {
+        @Override
+        public boolean isAllowed(FoodStatus first, FoodStatus second) {
+            return true;
+        }
+
+        @Override
+        public CalculationMode getCalculationMode(boolean hasMixedStatuses) {
+            return CalculationMode.AVERAGE;
+        }
+    },
+    SAME_STATUS {
+        @Override
+        public boolean isAllowed(FoodStatus first, FoodStatus second) {
+            return first == second;
+        }
+
+        @Override
+        public CalculationMode getCalculationMode(boolean hasMixedStatuses) {
+            return CalculationMode.AVERAGE;
+        }
+    },
+    WORST_STATUS {
+        @Override
+        public boolean isAllowed(FoodStatus first, FoodStatus second) {
+            return true;
+        }
+
+        @Override
+        public CalculationMode getCalculationMode(boolean hasMixedStatuses) {
+            return CalculationMode.WORST;
+        }
+    },
+    FRESH_STATUS {
+        @Override
+        public boolean isAllowed(FoodStatus first, FoodStatus second) {
+            return first == FoodStatus.FRESH && second == FoodStatus.FRESH;
+        }
+
+        @Override
+        public CalculationMode getCalculationMode(boolean hasMixedStatuses) {
+            return CalculationMode.AVERAGE;
+        }
+    },
+    FRESH_OR_STALE_STATUS {
+        @Override
+        public boolean isAllowed(FoodStatus first, FoodStatus second) {
+            return (first == FoodStatus.FRESH
+                    || first == FoodStatus.STALE)
+                    && (second == FoodStatus.FRESH
+                    || second == FoodStatus.STALE);
+        }
+
+        @Override
+        public CalculationMode getCalculationMode(boolean hasMixedStatuses) {
+            return hasMixedStatuses ? CalculationMode.WORST : CalculationMode.AVERAGE;
+        }
+    };
+
+    public abstract boolean isAllowed(FoodStatus first, FoodStatus second);
+
+    public abstract CalculationMode getCalculationMode(boolean hasMixedStatuses);
+
+    public enum CalculationMode {
+        AVERAGE,
+        WORST
+    }
 }
