1ca59436e9e061c4d1b21bdb675c71e356f95b0f
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-24 20:01:50 +0300

    Cosmetic changes

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/entity/ItemFrameMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/entity/ItemFrameMixin.java
@@ -1,19 +1,20 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.entity;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.item.ItemStack;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ItemFrame.class)
 public abstract class ItemFrameMixin {
-    @Inject(method = "setItem(Lnet/minecraft/world/item/ItemStack;Z)V", at = @At("TAIL"))
-    private void changeFoodEnvironmentWhenPlacedInItemFrame(CallbackInfo ci, @Local(argsOnly = true) ItemStack stackToPlaceInItemFrame) {
-        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceInItemFrame, FoodEnvironment.OPEN_AIR, ((ItemFrame)(Object)this).level());
+    @ModifyArg(method = "setItem(Lnet/minecraft/world/item/ItemStack;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/syncher/SynchedEntityData;set(Lnet/minecraft/network/syncher/EntityDataAccessor;Ljava/lang/Object;)V"), index = 1)
+    private Object changeFoodEnvironmentWhenPlacedInItemFrame(Object value) {
+        ItemStack stackToPlaceInItemFrame = (ItemStack) value;
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceInItemFrame, FoodEnvironment.OPEN_AIR, ((Entity)(Object)this).level());
+        return stackToPlaceInItemFrame;
     }
 }
