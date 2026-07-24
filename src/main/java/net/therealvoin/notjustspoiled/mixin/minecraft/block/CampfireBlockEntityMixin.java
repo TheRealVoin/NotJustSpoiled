b5d3f37a045acd323f7face563997ce1a2b08b28
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-24 17:31:06 +0300

    Slightly improved injection points & code readability

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
@@ -1,7 +1,5 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.block;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -11,23 +9,22 @@ import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(CampfireBlockEntity.class)
 public abstract class CampfireBlockEntityMixin {
-    @Inject(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.BEFORE))
-    private static void copySpoilageCapToCookedFood(CallbackInfo ci, @Local(argsOnly = true) Level level, @Local(ordinal = 0) ItemStack inputItem, @Local(ordinal = 1) ItemStack resultItem) {
-        FoodSpoilageManager.copySpoilage(inputItem, resultItem, level);
+    @ModifyArg(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"), index = 4)
+    private static ItemStack copySpoilageToCookedFood(ItemStack resultStack, @Local(argsOnly = true) Level level, @Local(ordinal = 0) ItemStack inputStack) {
+        FoodSpoilageManager.copySpoilage(inputStack, FoodEnvironment.COOKING, resultStack, level);
+        return resultStack;
     }
 
-    @WrapOperation(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;split(I)Lnet/minecraft/world/item/ItemStack;"))
-    private ItemStack changeFoodEnvironmentWhenPlacedOnCampfire(ItemStack stackInHand, int amount, Operation<ItemStack> original) {
-        ItemStack stackToPlaceOnCampfire = original.call(stackInHand, amount);
+    @ModifyArg(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;set(ILjava/lang/Object;)Ljava/lang/Object;"), index = 1)
+    private Object changeFoodEnvironmentWhenPlacedOnCampfire(Object value) {
+        ItemStack stackToPlaceOnCampfire = (ItemStack) value;
         CampfireBlockEntity campfire = (CampfireBlockEntity) (Object) this;
-        FoodEnvironment foodEnvironment = campfire.getBlockState().getValue(CampfireBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.OPEN_AIR;
-        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnCampfire, foodEnvironment, campfire.getLevel());
-
+        FoodEnvironment environment = campfire.getBlockState().getValue(CampfireBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.OPEN_AIR;
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnCampfire, environment, campfire.getLevel());
         return stackToPlaceOnCampfire;
     }
 }
