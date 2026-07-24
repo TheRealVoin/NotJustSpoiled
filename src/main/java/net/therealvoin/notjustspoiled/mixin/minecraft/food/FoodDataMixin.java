35780d8dbdff952acd4ce7bb03bbbbd30a889c37
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-24 16:02:09 +0300

    Fixed NPE, when somebody calls eat(Item, ItemStack) instead of eat(Item, ItemStack, LivingEntity)

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/food/FoodDataMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/food/FoodDataMixin.java
@@ -1,12 +1,14 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.food;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodStatus;
+import net.therealvoin.notjustspoiled.common.util.NJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -15,18 +17,20 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class FoodDataMixin {
     @ModifyArg(method = "eat(Lnet/minecraft/world/item/Item;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/food/FoodData;eat(IF)V"), index = 0)
     private int modifyFoodValuesAndApplyEffects(int defaultNutrition, @Local(argsOnly = true) ItemStack itemStack, @Local(argsOnly = true) LivingEntity entity) {
-        if (entity.level() instanceof ServerLevel serverLevel) {
-            FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(itemStack, serverLevel);
-            if (foodStatus == null) {
-                return defaultNutrition;
-            }
+        Level level = entity == null ? NJSUtils.getLevelWithoutContext() : entity.level();
 
-            FoodSpoilageManager.updateFoodLifetime(itemStack, serverLevel);
-            foodStatus.applyEffects(entity);
-
-            return foodStatus.getModifiedNutrition(defaultNutrition);
+        if (level.isClientSide()) {
+            return defaultNutrition;
         }
 
-        return defaultNutrition;
+        FoodStatus foodStatus = FoodSpoilageManager.getFoodStatus(itemStack, level);
+        if (foodStatus == null) {
+            return defaultNutrition;
+        }
+
+        FoodSpoilageManager.updateFoodLifetime(itemStack, FoodEnvironment.INVENTORY, level);
+        foodStatus.applyEffects(entity);
+
+        return foodStatus.getModifiedNutrition(defaultNutrition);
     }
 }
