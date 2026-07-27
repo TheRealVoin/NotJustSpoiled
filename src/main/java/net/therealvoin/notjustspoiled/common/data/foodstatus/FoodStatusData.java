5932cd85c75d161218c25f38b5b78b2a98179abe
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-27 12:29:40 +0300

    Renamed to FoodStatusEffectData

--- src/main/java/net/therealvoin/notjustspoiled/common/data/foodstatus/FoodStatusData.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/data/foodstatus/FoodStatusData.java
@@ -5,13 +5,13 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import java.util.List;
 
-public record FoodStatusData(double nutritionMultiplier, List<FoodStatusEffect> effects) {
+public record FoodStatusData(double nutritionMultiplier, List<FoodStatusEffectData> effects) {
     public static final Codec<FoodStatusData> CODEC = RecordCodecBuilder.create(
             instance -> instance.group(
                     Codec.doubleRange(0, 1)
                             .fieldOf("nutritionMultiplier")
                             .forGetter(FoodStatusData::nutritionMultiplier),
-                    FoodStatusEffect.CODEC
+                    FoodStatusEffectData.CODEC
                             .listOf()
                             .optionalFieldOf("effects", List.of())
                             .forGetter(FoodStatusData::effects)
