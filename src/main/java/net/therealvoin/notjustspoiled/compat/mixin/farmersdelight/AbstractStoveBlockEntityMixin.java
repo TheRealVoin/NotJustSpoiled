8999e0f1c802e96dc609467a190f2b4ca932eeae
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-30 17:09:37 +0300

    Removed COOKING environment as it doesn't really affect gameplay and makes code more complicated

--- src/main/java/net/therealvoin/notjustspoiled/compat/mixin/farmersdelight/AbstractStoveBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/compat/mixin/farmersdelight/AbstractStoveBlockEntityMixin.java
@@ -2,28 +2,27 @@ package net.therealvoin.notjustspoiled.compat.mixin.farmersdelight;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.items.ItemStackHandler;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import vectorwing.farmersdelight.common.block.AbstractStoveBlock;
 import vectorwing.farmersdelight.common.block.entity.AbstractStoveBlockEntity;
 
 @Mixin(AbstractStoveBlockEntity.class)
-public class AbstractStoveBlockEntityMixin {
+public abstract class AbstractStoveBlockEntityMixin {
     @Inject(method = "cookAndOutputItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isItemEnabled(Lnet/minecraft/world/flag/FeatureFlagSet;)Z", shift = At.Shift.BEFORE))
-    private void copySpoilageCapToCookedFood(CallbackInfo ci, @Local(name = "ingredient") ItemStack ingredient, @Local(name = "result") ItemStack result) {
-        FoodSpoilageManager.copySpoilage(ingredient, result, ((AbstractStoveBlockEntity)(Object)this).getLevel());
+    private void copySpoilageToCookedFood(CallbackInfo ci, @Local(name = "ingredient") ItemStack ingredient, @Local(name = "result") ItemStack result) {
+        FoodSpoilageManager.copySpoilage(ingredient, FoodEnvironment.OPEN_AIR, result, ((AbstractStoveBlockEntity)(Object)this).getLevel());
     }
 
-    @ModifyArg(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/ItemStackHandler;setStackInSlot(ILnet/minecraft/world/item/ItemStack;)V"), index = 1, remap = false)
-    private ItemStack updateFoodWhenPlacedOnStove(ItemStack stackToPlaceOnStove) {
-        FoodEnvironment foodEnvironment = ((AbstractStoveBlockEntity)(Object)this).getBlockState().getValue(AbstractStoveBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.OPEN_AIR;
-        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnStove, foodEnvironment, ((BlockEntity)(Object)this).getLevel());
-        return stackToPlaceOnStove;
+    @Inject(method = "serverTick", at = @At("TAIL"), remap = false)
+    private static void validateFoodEnvironment(CallbackInfo ci, @Local(argsOnly = true) AbstractStoveBlockEntity blockEntity) {
+        ItemStackHandler items = blockEntity.getItems();
+        for (int i = 0; i < items.getSlots(); i++) {
+            FoodSpoilageManager.changeEnvironmentAndUpdate(items.getStackInSlot(i), FoodEnvironment.OPEN_AIR, blockEntity.getLevel());
+        }
     }
 }
