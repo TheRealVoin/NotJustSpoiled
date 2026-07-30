23d0c8646f677d610b7179e877eac83a467730c1
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-30 11:17:30 +0300

    Removed COOKING environment as it doesn't really affect gameplay and makes code more complicated

--- src/main/java/net/therealvoin/notjustspoiled/compat/mixin/alexsmobs/MantisShrimpAIFryRiceMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/compat/mixin/alexsmobs/MantisShrimpAIFryRiceMixin.java
@@ -5,7 +5,6 @@ import com.github.alexthe666.alexsmobs.entity.ai.MantisShrimpAIFryRice;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Final;
@@ -22,19 +21,6 @@ public class MantisShrimpAIFryRiceMixin {
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/alexsmobs/entity/EntityMantisShrimp;setItemInHand(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.BEFORE))
     private void copySpoilageCapToResultStack(CallbackInfo ci, @Local(name = "rice") ItemStack riceStack) {
         ItemStack stackInHand = mantisShrimp.getItemInHand(InteractionHand.MAIN_HAND);
-        Level level = mantisShrimp.level();
-
-        FoodSpoilageManager.changeEnvironmentAndUpdate(stackInHand, FoodEnvironment.INVENTORY, level);
-        FoodSpoilageManager.copySpoilage(stackInHand, FoodEnvironment.INVENTORY, riceStack, level);
-    }
-
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void changeFoodEnvironmentWhenMantisShrimpStartsCooking(CallbackInfo ci) {
-        FoodSpoilageManager.changeEnvironmentAndUpdate(this.mantisShrimp.getItemInHand(InteractionHand.MAIN_HAND), FoodEnvironment.COOKING, this.mantisShrimp.level());
-    }
-
-    @Inject(method = "stop", at = @At("TAIL"))
-    private void changeFoodEnvironmentWhenMantisShrimpStopsCooking(CallbackInfo ci) {
-        FoodSpoilageManager.changeEnvironmentAndUpdate(this.mantisShrimp.getItemInHand(InteractionHand.MAIN_HAND), FoodEnvironment.INVENTORY, this.mantisShrimp.level());
+        FoodSpoilageManager.copySpoilage(stackInHand, FoodEnvironment.INVENTORY, riceStack, mantisShrimp.level());
     }
 }
