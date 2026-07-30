715b6dd4fd572e2622acf08d8f39f1840275c7cb
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-30 10:30:36 +0300

    Removed COOKING environment as it doesn't really affect gameplay and makes code more complicated

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/CampfireBlockEntityMixin.java
@@ -3,7 +3,6 @@ package net.therealvoin.notjustspoiled.mixin.minecraft.block;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
@@ -15,16 +14,14 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class CampfireBlockEntityMixin {
     @ModifyArg(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"), index = 4)
     private static ItemStack copySpoilageToCookedFood(ItemStack resultStack, @Local(argsOnly = true) Level level, @Local(ordinal = 0) ItemStack inputStack) {
-        FoodSpoilageManager.copySpoilage(inputStack, FoodEnvironment.COOKING, resultStack, level);
+        FoodSpoilageManager.copySpoilage(inputStack, FoodEnvironment.OPEN_AIR, resultStack, level);
         return resultStack;
     }
 
     @ModifyArg(method = "placeFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;set(ILjava/lang/Object;)Ljava/lang/Object;"), index = 1)
     private Object changeFoodEnvironmentWhenPlacedOnCampfire(Object value) {
         ItemStack stackToPlaceOnCampfire = (ItemStack) value;
-        CampfireBlockEntity campfire = (CampfireBlockEntity) (Object) this;
-        FoodEnvironment environment = campfire.getBlockState().getValue(CampfireBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.OPEN_AIR;
-        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnCampfire, environment, campfire.getLevel());
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceOnCampfire, FoodEnvironment.OPEN_AIR, ((CampfireBlockEntity)(Object)this).getLevel());
         return stackToPlaceOnCampfire;
     }
 }
