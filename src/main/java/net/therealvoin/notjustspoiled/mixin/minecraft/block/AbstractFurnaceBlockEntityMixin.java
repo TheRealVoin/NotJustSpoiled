f05489d35ae0c966b33c8c41675fc7198d118936
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-30 16:09:46 +0300

    Removed COOKING environment as it doesn't really affect gameplay and makes code more complicated, fully reworked food stacking system. Now it works perfectly!

--- src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/mixin/minecraft/block/AbstractFurnaceBlockEntityMixin.java
@@ -1,32 +1,54 @@
 package net.therealvoin.notjustspoiled.mixin.minecraft.block;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
+import net.therealvoin.notjustspoiled.common.util.NJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(value = AbstractFurnaceBlockEntity.class)
+@Mixin(AbstractFurnaceBlockEntity.class)
 public abstract class AbstractFurnaceBlockEntityMixin {
-    @Inject(method = "burn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V"))
-    private void copySpoilageCapToCookedFood(CallbackInfoReturnable<Boolean> cir, @Local(argsOnly = true) NonNullList<ItemStack> inventory) {
-        FoodSpoilageManager.copySpoilage(inventory.get(0), inventory.get(2), ((BlockEntity)(Object)this).getLevel());
+    @Shadow protected NonNullList<ItemStack> items;
+
+    @ModifyArgs(method = "setItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameTags(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private void removeSpoilageTagFromEqualityCheck(Args args) {
+        NJSUtils.removeSpoilageTagFromEqualityCheck(args, ((BlockEntity)(Object)this).getLevel());
     }
 
-    @Inject(method = "serverTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", shift = At.Shift.AFTER))
-    private static void changeFoodEnvironmentWhenFurnaceBecomesLitOrUnlit(Level level, BlockPos blockPos, BlockState blockState, AbstractFurnaceBlockEntity blockEntity, CallbackInfo ci) {
-        FoodEnvironment foodEnvironment = blockState.getValue(AbstractFurnaceBlock.LIT) ? FoodEnvironment.COOKING : FoodEnvironment.STORAGE;
-        FoodSpoilageManager.changeEnvironmentAndUpdate(blockEntity.getItem(0), foodEnvironment, level);
+    @Inject(method = "burn", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/crafting/Recipe;assemble(Lnet/minecraft/world/Container;Lnet/minecraft/core/RegistryAccess;)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER))
+    private void copySpoilageToCookedFood(CallbackInfoReturnable<Boolean> cir, @Local(argsOnly = true) NonNullList<ItemStack> inventory, @Local(ordinal = 0) ItemStack stackInCookSlot, @Local(ordinal = 1) ItemStack resultStack) {
+        FoodSpoilageManager.copySpoilage(stackInCookSlot, FoodEnvironment.STORAGE, resultStack, ((BlockEntity)(Object)this).getLevel());
+    }
+
+    @ModifyArg(method = "burn", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;set(ILjava/lang/Object;)Ljava/lang/Object;", ordinal = 0), index = 1)
+    private Object changeFoodEnvironmentWhenPlacedInResultSlot(Object value) {
+        ItemStack stackToPlaceInResultSlot = (ItemStack) value;
+        FoodSpoilageManager.changeEnvironmentAndUpdate(stackToPlaceInResultSlot, FoodEnvironment.STORAGE, ((BlockEntity)(Object)this).getLevel());
+        return stackToPlaceInResultSlot;
+    }
+
+    @WrapOperation(method = "burn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;grow(I)V"))
+    private void averageFoodLifetimeBeforeMerge(ItemStack stackInResultSlot, int increment, Operation<Void> originalMethod, @Local(ordinal = 1) ItemStack resultStack) {
+        FoodSpoilageManager.averageFoodLifetimeBeforeMerge(stackInResultSlot, FoodEnvironment.STORAGE, resultStack, FoodEnvironment.STORAGE, increment, ((BlockEntity)(Object)this).getLevel());
+        originalMethod.call(stackInResultSlot, increment);
+    }
+
+    @WrapOperation(method = "canBurn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private boolean addSpoilageTagToEqualityCheck(ItemStack stackInResultSlot, ItemStack resultStack, Operation<Boolean> originalMethod) {
+        Level level = ((BlockEntity)(Object)this).getLevel();
+        FoodSpoilageManager.copySpoilage(this.items.get(0), FoodEnvironment.STORAGE, resultStack, level);
+
+        return NJSUtils.addSpoilageTagToEqualityCheck(stackInResultSlot, resultStack, originalMethod, level);
     }
 }
