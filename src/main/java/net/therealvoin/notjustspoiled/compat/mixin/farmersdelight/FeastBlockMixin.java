cc20247b4284e0aab305274f7f87c1764beb3ca7
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-30 17:08:02 +0300

    Removed COOKING environment as it doesn't really affect gameplay and makes code more complicated

--- src/main/java/net/therealvoin/notjustspoiled/compat/mixin/farmersdelight/FeastBlockMixin.java
+++ src/main/java/net/therealvoin/notjustspoiled/compat/mixin/farmersdelight/FeastBlockMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.therealvoin.notjustspoiled.common.foodspoilage.BlockFoodSpoilage;
+import net.therealvoin.notjustspoiled.common.foodspoilage.FoodEnvironment;
 import net.therealvoin.notjustspoiled.common.foodspoilage.FoodSpoilageManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +21,7 @@ public class FeastBlockMixin {
     private void copySpoilageCapToFood(CallbackInfoReturnable<InteractionResult> cir, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockPos blockPos, @Local(name = "serving") ItemStack serving) {
         if (level instanceof ServerLevel serverLevel) {
             BlockFoodSpoilage data = BlockFoodSpoilage.get(serverLevel);
-            FoodSpoilageManager.copySpoilage(data.getLastItemStackAt(blockPos), serving, level);
+            FoodSpoilageManager.copySpoilage(data.getLastItemStackAt(blockPos), FoodEnvironment.OPEN_AIR, serving, level);
         }
     }
 }
