8886987a0eca456c9c6e4055df3e2d4d60e4da53
TheRealVoin <217010629+TheRealVoin@users.noreply.github.com>
2026-07-31 10:00:02 +0300

    Code improvements

--- src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodSpoilageManager.java
+++ src/main/java/net/therealvoin/notjustspoiled/common/foodspoilage/FoodSpoilageManager.java
@@ -1,7 +1,6 @@
 package net.therealvoin.notjustspoiled.common.foodspoilage;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,47 +8,57 @@ import net.therealvoin.notjustspoiled.common.network.DebugMessagePacket;
 
 public class FoodSpoilageManager {
     public static void changeEnvironmentAndUpdate(ItemStack itemStack, FoodEnvironment newFoodEnvironment, Level level) {
-        if (!(level instanceof ServerLevel serverLevel)) {
-            return;
-        }
-
         FoodSpoilage foodSpoilage = FoodSpoilage.of(itemStack);
 
         if (foodSpoilage == null || foodSpoilage.getEnvironment() == newFoodEnvironment) {
             return;
         }
 
-        updateFoodLifetime(itemStack, serverLevel);
+        FoodEnvironment currentEnvironment = foodSpoilage.isInitialized() ? foodSpoilage.getEnvironment() : newFoodEnvironment;
+
+        updateFoodLifetime(itemStack, currentEnvironment, level);
         foodSpoilage.setEnvironment(newFoodEnvironment);
 
-        DebugMessagePacket.sendToAll(Component.literal("environment: " + newFoodEnvironment));
+
+        if (!level.isClientSide()) {
+            DebugMessagePacket.sendToAll(Component.literal("environment: " + newFoodEnvironment));
+        }
     }
 
-    public static void updateFoodLifetime(ItemStack itemStack, ServerLevel serverLevel) {
-        long gameTime = serverLevel.getGameTime();
-
+    public static void updateFoodLifetime(ItemStack itemStack, FoodEnvironment currentEnvironment, Level level) {
         FoodSpoilage foodSpoilage = FoodSpoilage.of(itemStack);
 
-        if (foodSpoilage != null && !foodSpoilage.isInitialized()) {
-            foodSpoilage.setLastUpdateTime(gameTime);
-            DebugMessagePacket.sendToAll(Component.literal("Initialized food: " + ForgeRegistries.ITEMS.getKey(itemStack.getItem()).toString() + "\nlastUpdateTime: " + gameTime));
+        if (foodSpoilage == null) {
             return;
         }
+
+        if (!foodSpoilage.isInitialized()) {
+            initializeFood(foodSpoilage, currentEnvironment, level);
+            return;
+        }
+
+        long gameTime = level.getGameTime();
 
         if (foodSpoilage.getLastUpdateTime() == gameTime) {
             return;
         }
 
-        foodSpoilage.setFoodLifetime(calculateActualFoodLifetime(foodSpoilage, serverLevel));
+        foodSpoilage.setFoodLifetime(calculateActualFoodLifetime(foodSpoilage, level));
         foodSpoilage.setLastUpdateTime(gameTime);
 
-        DebugMessagePacket.sendToAll(Component.literal("Updated food: " + ForgeRegistries.ITEMS.getKey(itemStack.getItem()).toString() + "\nfoodLifetime: " + foodSpoilage.getFoodLifetime() + "\nlastUpdateTime: " + gameTime));
+        if (!level.isClientSide()) {
+            DebugMessagePacket.sendToAll(Component.literal("Updated food: " + ForgeRegistries.ITEMS.getKey(itemStack.getItem()).toString() + "\nfoodLifetime: " + foodSpoilage.getFoodLifetime() + "\nlastUpdateTime: " + gameTime));
+        }
     }
 
     public static FoodStatus getFoodStatus(ItemStack itemStack, Level level) {
         FoodSpoilage foodSpoilage = FoodSpoilage.of(itemStack);
-        if (foodSpoilage == null || !foodSpoilage.isInitialized()) {
+        if (foodSpoilage == null) {
             return null;
+        }
+
+        if (!foodSpoilage.isInitialized()) {
+            return FoodStatus.FRESH;
         }
 
         double foodLifetime = calculateActualFoodLifetime(foodSpoilage, level);
@@ -64,47 +73,37 @@ public class FoodSpoilageManager {
         return FoodStatus.SPOILED;
     }
 
-    public static void tryAverageSpoilageOnMerge(ItemStack itemStack1, ItemStack itemStack2, Level level) {
-        if (!(level instanceof ServerLevel serverLevel)) {
+    public static void averageFoodLifetimeBeforeMerge(ItemStack destinationStack, FoodEnvironment destinationEnvironment, ItemStack originStack, FoodEnvironment originEnvironment, int movedAmount, Level level) {
+        if (getFoodStatus(destinationStack, level) != getFoodStatus(originStack, level)) {
             return;
         }
 
-        if (itemStack1.isEmpty() || itemStack2.isEmpty() || !itemStack1.is(itemStack2.getItem())) {
-            return;
-        }
-
-        if (getFoodStatus(itemStack1, serverLevel) != getFoodStatus(itemStack2, serverLevel)) {
-            return;
-        }
-
-        FoodSpoilage foodSpoilage1 = FoodSpoilage.of(itemStack1);
-        FoodSpoilage foodSpoilage2 = FoodSpoilage.of(itemStack2);
+        FoodSpoilage foodSpoilage1 = FoodSpoilage.of(destinationStack);
+        FoodSpoilage foodSpoilage2 = FoodSpoilage.of(originStack);
 
         if (foodSpoilage1 == null || foodSpoilage2 == null) {
             return;
         }
 
-        updateFoodLifetime(itemStack1, serverLevel);
-        updateFoodLifetime(itemStack2, serverLevel);
+        updateFoodLifetime(destinationStack, destinationEnvironment, level);
+        updateFoodLifetime(originStack, originEnvironment, level);
 
-        int count1 = itemStack1.getCount();
-        int count2 = itemStack2.getCount();
-        double total = foodSpoilage1.getFoodLifetime() * count1 + foodSpoilage2.getFoodLifetime() * count2;
-        double average = total / (count1 + count2);
+        int destinationCount = destinationStack.getCount();
+
+        double total = foodSpoilage1.getFoodLifetime() * destinationCount + foodSpoilage2.getFoodLifetime() * movedAmount;
+        double average = total / (destinationCount + movedAmount);
 
         foodSpoilage1.setFoodLifetime(average);
         foodSpoilage2.setFoodLifetime(average);
+
+        DebugMessagePacket.sendToAll(Component.literal("Merged food: " + ForgeRegistries.ITEMS.getKey(destinationStack.getItem()).toString() + "\naverageFoodLifetime: " + average + "\ncount1: " + destinationCount + "\ncount2: " + movedAmount));
     }
 
     public static double calculateActualFoodLifetime(FoodSpoilage foodSpoilage, Level level) {
         return (level.getGameTime() - foodSpoilage.getLastUpdateTime()) * foodSpoilage.getEnvironment().getFoodSpoilageMultiplier() + foodSpoilage.getFoodLifetime();
     }
 
-    public static void copySpoilage(ItemStack copyFrom, ItemStack copyTo, Level level) {
-        if (!(level instanceof ServerLevel serverLevel)) {
-            return;
-        }
-
+    public static void copySpoilage(ItemStack copyFrom, FoodEnvironment copyFromEnvironment, ItemStack copyTo, Level level) {
         FoodSpoilage foodSpoilage1 = FoodSpoilage.of(copyFrom);
         FoodSpoilage foodSpoilage2 = FoodSpoilage.of(copyTo);
 
@@ -112,10 +111,18 @@ public class FoodSpoilageManager {
             return;
         }
 
-        updateFoodLifetime(copyFrom, serverLevel);
+        updateFoodLifetime(copyFrom, copyFromEnvironment, level);
         double spoilagePercent = foodSpoilage1.getFoodLifetime() / FoodCategory.of(copyFrom).getSpoilageTime();
         foodSpoilage2.setFoodLifetime(spoilagePercent * FoodCategory.of(copyTo).getSpoilageTime());
         foodSpoilage2.setEnvironment(foodSpoilage1.getEnvironment());
         foodSpoilage2.setLastUpdateTime(foodSpoilage1.getLastUpdateTime());
+    }
+
+    private static void initializeFood(FoodSpoilage foodSpoilage, FoodEnvironment foodEnvironment, Level level) {
+        foodSpoilage.setEnvironment(foodEnvironment);
+        foodSpoilage.setLastUpdateTime(level.getGameTime());
+        if (!level.isClientSide()) {
+            DebugMessagePacket.sendToAll(Component.literal("Initialized food: " + ForgeRegistries.ITEMS.getKey(foodSpoilage.getItemStack().getItem()).toString() + "\nlastUpdateTime: " + level.getGameTime()));
+        }
     }
 }
